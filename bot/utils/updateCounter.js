fcb6bbc17a242512d05b550942d7126dd34c09ca
eduardozgz <eduardozgz@protonmail.com>
2019-07-02 13:26:56 +0200

    bug fixes

--- bot/utils/updateCounter.js
+++ bot/utils/updateCounter.js
@@ -14,12 +14,14 @@ module.exports = (client, guild_id) => {
             });
 
             guild_config.channel_id.forEach(channel => {
-                client.channels.get(channel).setTopic(guild_config.topic.replace('{COUNT}', memberCountCustomized))
-                .catch( async (e)=>{
-                    error(e);
-                    const { error_no_perms } = require(`../lang/${((await getAvailableLanguages()).includes(guild_config.lang)) ? guild_config.lang : default_lang }.json`).functions.updateCounter;
-                    if(e.code && (e.code === 50013)) client.channels.get(channel).send(error_no_perms).catch(error);
-                });
+                if (client.channels.get(channel)) {
+                    client.channels.get(channel).setTopic(guild_config.topic.replace('{COUNT}', memberCountCustomized))
+                    .catch( async (e)=>{
+                        error(e);
+                        const { error_no_perms } = require(`../lang/${((await getAvailableLanguages()).includes(guild_config.lang)) ? guild_config.lang : default_lang }.json`).functions.updateCounter;
+                        if(e.code && (e.code === 50013)) client.channels.get(channel).send(error_no_perms).catch(error);
+                    });
+                }
             });
         }
     })
