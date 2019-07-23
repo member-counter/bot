214f11f4115d4d05c35f2435279f288acaade33e
eduardozgz <eduardozgz@protonmail.com>
2019-07-24 00:26:22 +0200

    sharding support

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 #!/usr/bin/env node
-
 require('dotenv').config();
-
-//bot
 const mongoose = require('mongoose');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const eventHandler = require('./bot/utils/eventHandler.js');
+const { ShardingManager } = require('discord.js');
 
-const mongodbUrl = process.env.DB_URI;
-const token = process.env.TOKEN;
-
-mongoose.connect(mongodbUrl, {useNewUrlParser: true}).then(()=>{console.log("Mongoose ready")}).catch((e)=>{console.error(e); process.exit(1)});
+mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}).then(()=>{console.log("[Main] Mongoose ready")}).catch((e)=>{console.error("[Main] " + e); process.exit(1)});
 mongoose.set('useFindAndModify', false);
 
-client.login(token);
-eventHandler(client);
+const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
 
-//discordbots.org
-const dbl = require("dblapi.js");
-
-const dblClient = new dbl(process.env.DBL_TOKEN, {statsInterval: 1800000}, client);
-
-dblClient.on('posted', () => {
-    console.log('[discordbots.org] Server count posted!');
-});
-  
-dblClient.on('error', e => {
-    console.error(`[discordbots.org] ${e}`)
-});
+manager.spawn();
+manager.on('launch', shard => console.log(`[Main] [Shard Manager] Launched shard #${shard.id}`));
+manager.on('message', message => console.log(`[Main] [Shard Manager] ${message}`));
