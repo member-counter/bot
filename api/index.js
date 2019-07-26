88582bf5968a00eea0254ff39e6df37639b94ecf
eduardozgz <eduardozgz@protonmail.com>
2019-07-26 23:10:57 +0200

    unnecessary if sentence

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdirSync(path.join(__dirname, 'routes')).forEach((file)=>{ 
-    if (file !== 'index.js') router.use(require(path.join(__dirname, 'routes', file)));
+    router.use(require(path.join(__dirname, 'routes', file)));
 });
 
 module.exports = router;
