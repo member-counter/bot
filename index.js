71cf10acebfbe9e4aaef749c26d7ebc60d9767c5
eduardozgz <eduardozgz@protonmail.com>
2019-08-10 23:30:33 +0200

    i forgot to push this

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Discord = require('discord.js');
-const https = require('https');
 const http = require('http');
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
-const compression = require('compression');
 const cors = require('cors');
 const path = require('path');
 
@@ -20,13 +17,8 @@ manager.spawn();
 manager.on('launch', shard => console.log(`[Main] [Shard Manager] Launched shard #${shard.id}`));
 
 //API
-//I'm not sure if this is the correct way to keep alaways 'available' a process but I use this on webscokets and it works fine
 const app = express();
-
-const options = {key: fs.readFileSync(process.env.PATH_PRIVATE_KEY, 'utf8'), cert: fs.readFileSync(process.env.PATH_PUBLIC_KEY, 'utf8')};
-
-const httpsServer = https.createServer(options, app).listen(process.env.PORT, () => console.log('[Main] [API] HTTPS server ready'));
-if (process.env.NODE_ENV === "development") http.createServer(app).listen(80, () => console.log('[Main] [API] HTTP server ready'));
+http.createServer(app).listen(process.env.PORT, () => console.log('[Main] [API] HTTP server ready'));
 
 app.use((req, res, next) => {
     req.DiscordShardManager = manager;
@@ -34,9 +26,6 @@ app.use((req, res, next) => {
 })
 
 app.use(bodyParser.json());
-
-//gzip
-app.use(compression());
 
 //just to avoid problems while I'm developing this
 if (process.env.NODE_ENV === "development") app.use(cors());
