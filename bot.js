29304e01f8f4cf3f0352636f814eca443a3c41e3
eduardozgz <eduardozgz@protonmail.com>
2019-08-18 17:51:52 +0200

    bug fixes

--- bot.js
+++ bot.js
@@ -21,13 +21,7 @@ mongoose.connect(process.env.DB_URI, {useNewUrlParser: true})
 mongoose.set('useFindAndModify', false);
 
 //DiscordBots.org
-const dblClient = new dbl(process.env.DBL_TOKEN);
-
-dblClient.on('ready', () => {
-    setInterval(() => {
-        dbl.postStats(client.guilds.size, client.shards.Id, client.shards.total);
-    }, 5*60*1000);
-});
+const dblClient = new dbl(process.env.DBL_TOKEN, client);
 
 dblClient.on('posted', () => {
     console.log(`[Bot shard #${client.shard.id}] [discordbots.org client] Server count posted!`);
