116356e1f839e6870049417f7802ccab97953d46
eduardozgz <eduardozgz@protonmail.com>
2019-08-18 15:58:47 +0200

    improvements

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -5,13 +5,22 @@ const command = {
     commands: [prefix+"help"],
     indexZero: true,
     enabled: true,
-    run: (client, message, language) => {
+    run: (client, message, language) => {        
         const args = message.content.split(' ');
         if (args.length < 2) {
             const embed = language.commands.help.embed_reply;
             message.channel.send({ embed }).catch(console.error);
         } else {
-            const selectedCommand = language.commands[args[1]]
+            Object.entries(language.commands)
+                .map(x => {
+                    x[0] = x[0].toLowerCase();
+                    return x
+                })
+                .forEach(x => {
+                    language.commands[x[0]] = x[1]
+                })
+
+            const selectedCommand = language.commands[args[1].toLowerCase()]
             if (selectedCommand) {
                 const embed = {
                     "title": language.commands.help.misc.command + " " + args[1],
