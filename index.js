8bb93f4a3b95cd65a1dba18702d71e554b3b4f74
eduardozgz <eduardozgz@protonmail.com>
2019-08-22 17:37:59 +0200

    prettified

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 #!/usr/bin/env node
-require('dotenv').config();
-const mongoose = require('mongoose');
-const Discord = require('discord.js');
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Discord = require("discord.js");
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}).then(()=>{console.log("[Main] Mongoose ready")}).catch((e)=>{console.error("[Main] " + e); process.exit(1)});
-mongoose.set('useFindAndModify', false);
+mongoose
+    .connect(process.env.DB_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log("[Main] Mongoose ready");
+    })
+    .catch(e => {
+        console.error("[Main] " + e);
+        process.exit(1);
+    });
+mongoose.set("useFindAndModify", false);
 
-const manager = new Discord.ShardingManager('./bot.js', { token: process.env.DISCORD_TOKEN });
+const manager = new Discord.ShardingManager("./bot.js", {
+    token: process.env.DISCORD_TOKEN
+});
 
 manager.spawn();
-manager.on('launch', shard => console.log(`[Main] [Shard Manager] Launched shard #${shard.id}`));
+manager.on("launch", shard =>
+    console.log(`[Main] [Shard Manager] Launched shard #${shard.id}`)
+);
 
 //API
 const app = express();
-http.createServer(app).listen(process.env.PORT, () => console.log('[Main] [API] HTTP server ready'));
+http.createServer(app).listen(process.env.PORT, () =>
+    console.log("[Main] [API] HTTP server ready. Port: " + process.env.PORT)
+);
 
 app.use((req, res, next) => {
     req.DiscordShardManager = manager;
     next();
-})
+});
 
 app.use(bodyParser.json());
 
@@ -31,4 +44,4 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "development") app.use(cors());
 
 //routes
-app.use(`/api`, require('./api/index'));
+app.use(`/api`, require("./api/index"));
