2355fa33a0eeecc5bb3458d6136caee0e0f7eb75
eduardozgz <eduardozgz@protonmail.com>
2019-08-24 23:59:31 +0200

    improvements

--- bot/utils/updateCounter.js
+++ bot/utils/updateCounter.js
@@ -15,7 +15,7 @@ module.exports = (client, guild_id) => {
 
                     guild_config.enabled_channels.forEach(channel => {
                         if (client.channels.get(channel)) {
-                            client.channels.get(channel).setTopic(guild_config.topic.replace('{COUNT}', memberCountCustomized))
+                            client.channels.get(channel).setTopic(guild_config.topic.replace(/\{COUNT\}/gi, memberCountCustomized))
                             .catch( async (e) => {
                                 console.error(e);
                                 const { error_no_perms } = require(`../lang/${((await getAvailableLanguages()).includes(guild_config.lang)) ? guild_config.lang : default_lang }.json`).functions.updateCounter;
