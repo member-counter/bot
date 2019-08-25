506968d09b6f6396843187a16f71d3c402c7a913
eduardozgz <eduardozgz@protonmail.com>
2019-08-25 19:52:45 +0200

    bug fixes

--- api/routes/dbl.js
+++ api/routes/dbl.js
@@ -3,7 +3,7 @@ const dblSecret = process.env.DBL_WH_SECRET;
 const officalServer = process.env.DISCORD_OFFICIAL_SERVER_ID;
 const rewardRoleId = process.env.DBL_REWARD_ROLE_ID;
 
-router.get("/dbl", (req, res) => {
+router.post("/dbl", (req, res) => {
     const { authorization } = req.headers;
     const webhook = req.body;
     if (authorization === dblSecret && webhook.type === "upvote" && webhook.bot === process.env.DISCORD_CLIENT_ID) {
