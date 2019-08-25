43786d84da5633c699e707cf69c578e0c6c2a1f8
eduardozgz <eduardozgz@protonmail.com>
2019-08-25 21:09:47 +0200

    improvements

--- api/routes/dbl.js
+++ api/routes/dbl.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const dblSecret = process.env.DBL_WH_SECRET;
-const officalServer = process.env.DISCORD_OFFICIAL_SERVER_ID;
+const officialServer = process.env.DISCORD_OFFICIAL_SERVER_ID;
 const rewardRoleId = process.env.DBL_REWARD_ROLE_ID;
+const owners = process.env.BOT_OWNERS.split(/,\s?/);
 
 router.post("/dbl", (req, res) => {
     const { authorization } = req.headers;
@@ -9,18 +10,35 @@ router.post("/dbl", (req, res) => {
     if (authorization === dblSecret && webhook.type === "upvote" && webhook.bot === process.env.DISCORD_CLIENT_ID) {
         console.log(`[MAIN] [API] User ${webhook.user} upvoted the bot!`);
         req.DiscordShardManager.broadcastEval(`
-            this.guilds.get(${officalServer})
-                .members.get(${webhook.user})
-                    .addRole(${rewardRoleId})
-                        .then(() => {
-                            console.log("[MAIN] [API] Role given successfully to ${webhook.user}");
-                        })
-                        .catch((e) => {
-                            console.error("[MAIN] [API] Error while trying to give the role to ${webhook.user}");
-                        });
+            if (this.guilds.get(${officialServer})) {
+                this.guilds.get(${officialServer})
+                    .members.get(${webhook.user})
+                        .addRole(${rewardRoleId})
+                            .then(() => {
+                                console.log("[MAIN] [API] Role given successfully to ${webhook.user}");
+                            })
+                            .catch((e) => {
+                                console.error("[MAIN] [API] Error while trying to give the role to ${webhook.user}");
+                            });
+            }
         `);
     } else if (authorization === dblSecret && webhook.type === "test" && webhook.bot === process.env.DISCORD_CLIENT_ID) {
         console.log(`[MAIN] [API] DBL webhook test received: ${JSON.stringify(webhook)}`);
+        if (owners.includes(webhook.user)) {
+            req.DiscordShardManager.broadcastEval(`
+            if (this.guilds.get(${officialServer})) {
+                this.guilds.get(${officialServer})
+                    .members.get(${webhook.user})
+                        .addRole(${rewardRoleId})
+                            .then(() => {
+                                console.log("[MAIN] [API] Role given successfully to ${webhook.user}");
+                            })
+                            .catch((e) => {
+                                console.error("[MAIN] [API] Error while trying to give the role to ${webhook.user}");
+                            });
+            }
+        `);
+        }
     }
 });
 
