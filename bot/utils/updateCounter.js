a2710036db87fcd26f0b7a4b90e50cf2fdc017a3
eduardozgz <eduardozgz@protonmail.com>
2019-08-25 23:39:26 +0200

    removed an annoying error

--- bot/utils/updateCounter.js
+++ bot/utils/updateCounter.js
@@ -3,6 +3,7 @@ const default_lang = process.env.DISCORD_DEFAULT_LANG;
 const { getAvailableLanguages } = require('../utils/language');
 module.exports = (client, guild_id) => {
     if (client.guilds.get(guild_id)) {
+        console.log(`[Bot shard #${client.shard.id}] Updating ${guild_id}'s counter, ${client.guilds.get(guild_id).memberCount} members.`)
         GuildModel.findOne({guild_id})
             .then((guild_config) => {
                 if (guild_config && (guild_config.enabled_channels.length !== 0)) {
@@ -16,11 +17,10 @@ module.exports = (client, guild_id) => {
                     guild_config.enabled_channels.forEach(channel => {
                         if (client.channels.get(channel)) {
                             client.channels.get(channel).setTopic(guild_config.topic.replace(/\{COUNT\}/gi, memberCountCustomized))
-                            .catch( async (e) => {
-                                console.error(e);
-                                const { error_no_perms } = require(`../lang/${((await getAvailableLanguages()).includes(guild_config.lang)) ? guild_config.lang : default_lang }.json`).functions.updateCounter;
-                                if(e.code && (e.code === 50013)) client.channels.get(channel).send(error_no_perms).catch(console.error);
-                            });
+                                .catch( async (e) => {
+                                    const { error_no_perms } = require(`../lang/${((await getAvailableLanguages()).includes(guild_config.lang)) ? guild_config.lang : default_lang }.json`).functions.updateCounter;
+                                    if (e.code && (e.code === 50013)) client.channels.get(channel).send(error_no_perms).catch(console.error);
+                                });
                         }
                     });
                 }
