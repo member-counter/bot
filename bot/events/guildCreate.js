d13b4e461a92409a051818d6a68cbab9767d28d6
eduardozgz <eduardozgz@protonmail.com>
2019-08-31 17:38:12 +0200

    Join/Left event

---
+++ bot/events/guildCreate.js
@@ -0,0 +1,5 @@
+module.exports = (client) => {
+    client.on('guildCreate', (guild) => {
+        console.log(`[Bot shard #${client.shard.id}] Joined guild ${guild.name} (${guild.id})`);
+    });
+}
