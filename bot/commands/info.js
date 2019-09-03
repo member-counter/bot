5c67b7f04fe6a8b80f665eb90ea95b34f2cdc86a
eduardozgz <eduardozgz@protonmail.com>
2019-09-03 17:36:29 +0200

    code cleanup

--- bot/commands/info.js
+++ bot/commands/info.js
@@ -2,14 +2,20 @@ const prefix = process.env.DISCORD_PREFIX;
 
 const info = {
     name: "info",
-    commands: [prefix+"invite", prefix+"info", prefix+"github", prefix+"support", prefix+"bug"],
+    commands: [
+        prefix + "invite",
+        prefix + "info",
+        prefix + "github",
+        prefix + "support",
+        prefix + "bug"
+    ],
     allowedTypes: ["text", "dm"],
     indexZero: true,
     enabled: true,
     run: (client, message, translation) => {
-		const embed = translation.commands.info.embed_reply;
-		message.channel.send({embed}).catch(console.error);
+        const embed = translation.commands.info.embed_reply;
+        message.channel.send({ embed }).catch(console.error);
     }
-}
+};
 
-module.exports = [ info ];
+module.exports = [info];
