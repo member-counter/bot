bb2f75e9df50c96f88b79e03d1e216a1e67fe479
eduardozgz <eduardozgz@protonmail.com>
2019-09-10 01:02:33 +0200

    code cleanup

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
 
-fs.readdirSync(path.join(__dirname, 'routes')).forEach((file) => {
-    router.use(require(path.join(__dirname, 'routes', file)));
+fs.readdirSync(path.join(__dirname, "routes")).forEach(file => {
+    router.use(require(path.join(__dirname, "routes", file)));
 });
 
 module.exports = router;
--- api/v1/routes/ping.js
+++ api/v1/routes/ping.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.all('/ping', (req, res) => {
-    res.json({ pong: new Date() })
+router.all("/ping", (req, res) => {
+    res.json({ pong: new Date() });
 });
 
 module.exports = router;
