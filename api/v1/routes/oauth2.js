858c8f0bea0ff5325b6818501c12e42f763148e6
eduardozgz <eduardozgz@protonmail.com>
2019-09-10 01:03:24 +0200

    code cleanup

--- api/v1/routes/oauth2.js
+++ api/v1/routes/oauth2.js
@@ -2,20 +2,20 @@ const router = require("express").Router();
 const fetch = require("node-fetch");
 const owners = process.env.BOT_OWNERS.split(/,\s?/);
 const auth = require("../middlewares/auth");
-const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } = process.env;
+const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_OAUTH2_URL_REDIRECT } = process.env;
 
 router.get("/oauth2", async (req, res) => {
     const accessCode = req.query.code;
     let data = new URLSearchParams();
     data.append("grant_type", "authorization_code");
     data.append("code", accessCode);
-    data.append("redirect_uri", process.env.DISCORD_OAUTH2_URL_REDIRECT);
+    data.append("redirect_uri", DISCORD_OAUTH2_URL_REDIRECT);
     data.append("scope", "identify guilds");
 
     fetch(`https://discordapp.com/api/oauth2/token?${data}`, {
         method: "POST",
         headers: {
-            "Authorization": "Basic " + Buffer.from(DISCORD_CLIENT_ID + ":" + process.env.DISCORD_CLIENT_SECRET).toString("base64"),
+            "Authorization": "Basic " + Buffer.from(DISCORD_CLIENT_ID + ":" + DISCORD_CLIENT_SECRET).toString("base64"),
             "Content-Type": "application/x-www-form-urlencoded"
         }
     })
