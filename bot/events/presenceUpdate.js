efcbfbac2b6155972c85603b4a2c137bab8b0e04
eduardozgz <eduardozgz@protonmail.com>
2019-09-22 01:59:47 +0200

    optimizations, I think

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,7 +1,18 @@
 const updateCounter = require("../utils/updateCounter");
+const wait = require("../utils/wait");
+
+const TIME_BETWEEN_EVERY_UPDATE = 1 * 1000;
+const GuildsToUpdate = new Map();
 
 module.exports = client => {
     client.on("presenceUpdate", (oldMember, newMember) => {
-        if (oldMember.presence.status !== newMember.presence.status) updateCounter(client, newMember.guild.id, ["onlineusers", "offlineusers"]);
+        if (oldMember.presence.status !== newMember.presence.status) GuildsToUpdate.set(newMember.guild.id);
     });
+
+    setInterval(() => {
+        GuildsToUpdate.forEach(async (value, guild_id) => {
+            updateCounter(client, guild_id, ["onlineusers", "offlineusers"]);
+        });
+        GuildsToUpdate.clear();
+    }, TIME_BETWEEN_EVERY_UPDATE);
 };
