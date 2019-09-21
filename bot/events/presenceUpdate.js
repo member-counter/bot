a4ee12b51d156dba03f4cbb5a9c7ed17260a6c05
eduardozgz <eduardozgz@protonmail.com>
2019-09-21 19:44:25 +0200

    this should work

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,7 +1,7 @@
 const updateCounter = require("../utils/updateCounter");
 
 module.exports = client => {
-    client.on("presenceUpdate", () => {
-        //updateCounter(client, channel.guild.id, ["onlineusers", "offlineusers"]);
+    client.on("presenceUpdate", (oldMember, newMember) => {
+        updateCounter(client, oldMember.guild.id, ["onlineusers", "offlineusers"]);
     });
 };
