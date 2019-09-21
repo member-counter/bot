f7d68f286111f6fc628207d75d3e41b1bdd60954
eduardozgz <eduardozgz@protonmail.com>
2019-09-21 23:12:03 +0200

    fix

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,7 +1,17 @@
 const updateCounter = require("../utils/updateCounter");
 
+const TIME_BETWEEN_EVERY_UPDATE = 10 * 1000;
+const GuildsToUpdate = new Map();
+
 module.exports = client => {
     client.on("presenceUpdate", (oldMember, newMember) => {
-        updateCounter(client, newMember.guild.id, ["onlineusers", "offlineusers"]);
+        GuildsToUpdate.set(newMember.guild.id);
     });
+
+    setInterval(() => {
+        GuildsToUpdate.forEach((value, guild_id) => {
+            updateCounter(client, guild_id, ["onlineusers", "offlineusers"]);
+        });
+        GuildsToUpdate.clear();
+    }, TIME_BETWEEN_EVERY_UPDATE);
 };
