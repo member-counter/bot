be26e6bb15c209638a3623e03f2ddba2849c5915
eduardozgz <eduardozgz@protonmail.com>
2019-09-21 23:25:40 +0200

    avoid cpu peaks

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,7 +1,8 @@
 const updateCounter = require("../utils/updateCounter");
 
-const TIME_BETWEEN_EVERY_UPDATE = 10 * 1000;
+const TIME_BETWEEN_EVERY_UPDATE = 20 * 1000;
 const GuildsToUpdate = new Map();
+const wait = ms => new Promise((resolve)=>setTimeout(resolve, ms));
 
 module.exports = client => {
     client.on("presenceUpdate", (oldMember, newMember) => {
@@ -9,8 +10,9 @@ module.exports = client => {
     });
 
     setInterval(() => {
-        GuildsToUpdate.forEach((value, guild_id) => {
+        GuildsToUpdate.forEach(async (value, guild_id) => {
             updateCounter(client, guild_id, ["onlineusers", "offlineusers"]);
+            await wait(TIME_BETWEEN_EVERY_UPDATE / client.guilds.size);
         });
         GuildsToUpdate.clear();
     }, TIME_BETWEEN_EVERY_UPDATE);
