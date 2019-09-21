150d909fec42eb7c713708332ef500d4889c8c63
eduardozgz <eduardozgz@protonmail.com>
2019-09-21 19:32:20 +0200

    WIP

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,7 +1,7 @@
 const updateCounter = require("../utils/updateCounter");
 
 module.exports = client => {
-    client.on("presenceUpdate", channel => {
-        updateCounter(client, channel.guild.id, ["onlineusers", "offlineusers"]);
+    client.on("presenceUpdate", () => {
+        //updateCounter(client, channel.guild.id, ["onlineusers", "offlineusers"]);
     });
 };
