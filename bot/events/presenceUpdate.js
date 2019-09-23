9c5bccf1e81f90dda34455215b3e5f7b1b7d7feb
eduardozgz <eduardozgz@protonmail.com>
2019-09-24 01:42:31 +0200

    this might work

--- bot/events/presenceUpdate.js
+++ bot/events/presenceUpdate.js
@@ -1,18 +1,17 @@
 const updateCounter = require("../utils/updateCounter");
-const wait = require("../utils/wait");
 
-const TIME_BETWEEN_EVERY_UPDATE = 1 * 1000;
+const TIME_BETWEEN_EVERY_UPDATE = 10 * 1000;
 const GuildsToUpdate = new Map();
 
 module.exports = client => {
     client.on("presenceUpdate", (oldMember, newMember) => {
-        if (oldMember.presence.status !== newMember.presence.status) GuildsToUpdate.set(newMember.guild.id);
+        if (oldMember.presence.status !== "offline") oldMember.presence.status = "online";
+        if (newMember.presence.status !== "offline") newMember.presence.status = "online";
+        if (oldMember.presence.status !== newMember.presence.status) {
+            if (!GuildsToUpdate.has(newMember.guild.id)) GuildsToUpdate.set(newMember.guild.id, setTimeout(() => {
+                updateCounter(client, newMember.guild.id, ["onlineusers", "offlineusers"]);
+                GuildsToUpdate.delete(newMember.guild.id);
+            }, TIME_BETWEEN_EVERY_UPDATE));
+        }
     });
-
-    setInterval(() => {
-        GuildsToUpdate.forEach(async (value, guild_id) => {
-            updateCounter(client, guild_id, ["onlineusers", "offlineusers"]);
-        });
-        GuildsToUpdate.clear();
-    }, TIME_BETWEEN_EVERY_UPDATE);
 };
