fb03df5df9a54ff8d6e2c61d833c858c96731844
eduardozgz <eduardozgz@protonmail.com>
2019-09-24 00:19:35 +0200

    onlineusers and offlineusers disabled until I fix them

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const updateCounter = require("./bot/utils/updateCounter");
 global.spawnedAt = new Date();
 
 const client = new Discord.Client({
-    disabledEvents: ["TYPING_START"]
+    disabledEvents: ["TYPING_START", "PRESENCE_UPDATE"]
 });
 
 client.login(process.env.DISCORD_TOKEN);
