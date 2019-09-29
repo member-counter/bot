4448640c0f632c645c1ec08138c4dab39d6891d1
eduardozgz <eduardozgz@protonmail.com>
2019-09-29 22:48:17 +0200

    code refactoring (wip)

--- bot/commands/info.js
+++ bot/commands/info.js
@@ -10,7 +10,7 @@ const info = {
     allowedTypes: ["text", "dm"],
     indexZero: true,
     enabled: true,
-    run: ({ message, guild_settings, translation }) => {
+    run: ({ message, translation }) => {
         const embed = translation.commands.info.embed_reply;
         message.channel.send({ embed }).catch(console.error);
     }
