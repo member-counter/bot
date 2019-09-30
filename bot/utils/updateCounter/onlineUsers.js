0b1bcfd8842652795d23f60182c72e11cca0eca2
eduardozgz <eduardozgz@protonmail.com>
2019-10-01 01:57:43 +0200

    code refactoring

--- bot/utils/updateCounter/onlineUsers.js
+++ bot/utils/updateCounter/onlineUsers.js
@@ -1,4 +1,4 @@
-const removeChannelFromDb = require("../removeChannelFromDb");
+const setChannelName = require("./functions/setChannelName");
 
 module.exports = (client, guildSettings) => {
     const {
@@ -11,16 +11,7 @@ module.exports = (client, guildSettings) => {
         const count = client.guilds.get(guild_id).members.filter(member => member.presence.status !== "offline").size;
         channelNameCounter.forEach((channel_name, channel_id) => {
             if (channelNameCounter_types.has(channel_id) && (channelNameCounter_types.get(channel_id) === "onlineusers"))
-                if (client.channels.has(channel_id)) {
-                    const nameToSet = channel_name.replace(/\{COUNT\}/gi, count);
-                    client.channels
-                        .get(channel_id)
-                        .setName(nameToSet)
-                        .catch(error => {
-                            removeChannelFromDb({ client, guildSettings, error, channel: channel_id, type: "channelNameCounter" });
-                            console.error(error);
-                        });
-                }
+                setChannelName({ client, guildSettings, channelId: channel_id, channelName: channel_name, count });
         });
     }
 };
