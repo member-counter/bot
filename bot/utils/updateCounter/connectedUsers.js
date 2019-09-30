cbf012f24b0f531e24330011523ca1e9803cb5a5
eduardozgz <eduardozgz@protonmail.com>
2019-10-01 00:40:07 +0200

    code refactoring
    Former-commit-id: d09f102bc27e2e99c26158e3a371623ddcedecf5

--- bot/utils/updateCounter/connectedUsers.js
+++ bot/utils/updateCounter/connectedUsers.js
@@ -1,3 +1,5 @@
+const removeChannelFromDb = require("../removeChannelFromDb");
+
 module.exports = (client, guildSettings) => {
     const {
         guild_id,
@@ -17,15 +19,9 @@ module.exports = (client, guildSettings) => {
                     client.channels
                         .get(channel_id)
                         .setName(nameToSet)
-                        .catch(e => {
-                            //errors caused by permissions
-                            if (e.code === 50013 || e.code === 50001)
-                                console.log(
-                                    `[Bot shard #${client.shard.id}] I tried to update ${guild_id}'s counter, but I don't have the proper permissions. Error code: ${e.code}`
-                                );
-                            else {
-                                console.error(e);
-                            }
+                        .catch(error => {
+                            removeChannelFromDb({ client, guildSettings, error, channel: channel_id, type: "channelNameCounter" });
+                            console.error(error);
                         });
                 }
         });
