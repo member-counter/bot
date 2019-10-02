5c4b925ee504b9cd0fdbc6ba70025c68cae2bee7
eduardozgz <eduardozgz@protonmail.com>
2019-10-02 16:42:31 +0200

    I'm gonna probably crash the bot

--- bot/events/guildDelete.js
+++ bot/events/guildDelete.js
@@ -1,5 +1,5 @@
 module.exports = client => {
     client.on('guildDelete', guild => {
-        console.log(`[Bot shard #${client.shard.id}] Left guild ${guild.name} (${guild.id})`);
+        console.log(`[Bot shard #${client.shard.id}] Left guild ${guild.name} (${guild.id}), shard guilds-users: ${client.guilds.size}-${client.users.size}`);
     });
 };
