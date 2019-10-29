425c7982e1f550336be05dc512f381cd43d79f25
eduardozgz <eduardozgz@protonmail.com>
2019-10-29 12:55:33 +0100

    Bug fixes and added stats post for discord.boats and discord.bots.gg
    Former-commit-id: 88aa53ae8a36e53a4cff1cf02322a84b6c93a9ee

--- api/v1/routes/dbl.js
+++ api/v1/routes/dbl.js
@@ -1,54 +1,20 @@
 const router = require("express").Router();
-const UserModel = require("../../../mongooseModels/UserModel");
-const {
-    DISCORD_CLIENT_ID,
-    DISCORD_OFFICIAL_SERVER_ID,
-    DBL_WH_SECRET,
-    DBL_REWARD_ROLE_ID
-} = process.env;
+const giveUpvoterRole = require("../../../bot/utils/giveUpvoterRole");
+const { DBL_WH_SECRET } = process.env;
 const owners = process.env.BOT_OWNERS.split(/,\s?/);
 
 router.post("/dbl", (req, res) => {
     const { authorization } = req.headers;
     const webhook = req.body;
-    if (
-        authorization === DBL_WH_SECRET &&
-        webhook.type === "upvote" &&
-        webhook.bot === DISCORD_CLIENT_ID
-    ) {
-        res.status(200);
-        console.log(`[MAIN] [API] User ${webhook.user} upvoted the bot!`);
-        UserModel.findOneAndUpdate({ user_id: webhook.user }, { voted_dbl: true }, { upsert: true }).exec().catch(console.error);
+    if (authorization === DBL_WH_SECRET && webhook.type === "upvote") {
+        console.log(`[MAIN] [API] [TOP.GG] User ${webhook.user} upvoted the bot!`);
         giveUpvoterRole(webhook.user, req.DiscordShardManager);
-    } else if (
-        authorization === DBL_WH_SECRET &&
-        webhook.type === "test" &&
-        webhook.bot === DISCORD_CLIENT_ID
-    ) {
         res.status(200);
-        console.log(
-            `[MAIN] [API] DBL webhook test received: ${JSON.stringify(webhook)}`
-        );
-        if (owners.includes(webhook.user)) {
-            giveUpvoterRole(webhook.user, req.DiscordShardManager);
-        }
+    } else if (authorization === DBL_WH_SECRET && webhook.type === "test") {
+        console.log(`[MAIN] [API] [TOP.GG] Webhook test received: ${JSON.stringify(webhook)}`);
+        giveUpvoterRole(webhook.user, req.DiscordShardManager);
+        res.status(200);
     } else res.status(401);
 });
 
 module.exports = router;
-
-const giveUpvoterRole = (user, DiscordShardManager) => {
-    DiscordShardManager.broadcastEval(`
-        if (this.guilds.get("${DISCORD_OFFICIAL_SERVER_ID}") && this.guilds.get("${DISCORD_OFFICIAL_SERVER_ID}").members.get("${user}")) {
-            this.guilds.get("${DISCORD_OFFICIAL_SERVER_ID}")
-                .members.get("${user}")
-                    .addRole("${DBL_REWARD_ROLE_ID}")
-                        .then(() => {
-                            console.log("[MAIN] [API] Upvoter Role given successfully to ${user}");
-                        })
-                        .catch(error => {
-                            console.error("[MAIN] [API] Error while trying to give the role to ${user}, Error code: " + error.code);
-                        });
-        }
-    `);
-}
---
+++ bot/utils/botStatsSender.js
@@ -0,0 +1,52 @@
+const INTERVAL_STATS_TIME = 15 * 60 * 60;
+const { DISCORD_CLIENT_ID, DBL_TOKEN, DBGG_TOKEN, DBOATS_TOKEN } = process.env;
+//const fetch = require("node-fetch");
+const getTotalGuildsAndMembers = require("./getTotalGuildsAndMembers");
+
+module.exports = client => {
+    if (client.shard.id === 0) {
+        setInterval(async () => {
+            const guildCount = await getTotalGuildsAndMembers(client).totalGuilds;
+
+            //https://discord.bots.gg
+            fetch(`https://discord.bots.gg/api/v1/bots/${DISCORD_CLIENT_ID}/stats`, {
+                method: "post",
+                headers: {
+                    Authorization: DBGG_TOKEN,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ guildCount })
+            })
+                .then(response => response.json())
+                .then(console.log)
+                .catch(console.error);
+
+            //https://top.gg/
+            fetch(`https://top.gg/api/bots/${DISCORD_CLIENT_ID}/stats`, {
+                method: "post",
+                headers: {
+                    Authorization: DBL_TOKEN,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ server_count: guildCount })
+            })
+                .then(response => response.json())
+                .then(console.log)
+                .catch(console.error);
+
+            //https://discord.boats/
+            fetch(`https://discord.boats/api/v2/bot/${DISCORD_CLIENT_ID}`, {
+                method: "post",
+                headers: {
+                    Authorization: DBOATS_TOKEN,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ server_count: guildCount })
+            })
+                .then(response => response.json())
+                .then(console.log)
+                .catch(console.error);
+
+        }, INTERVAL_STATS_TIME);
+    }
+};
