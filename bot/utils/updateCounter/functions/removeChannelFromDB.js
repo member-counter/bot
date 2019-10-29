8732fe243018652ea2353a9206194ba9093f8ddb
eduardozgz <eduardozgz@protonmail.com>
2019-10-29 13:04:53 +0100

    Bug fix
    Former-commit-id: f732728f962f9560338f0a4f2470b4683ca308fb

--- bot/utils/updateCounter/functions/removeChannelFromDB.js
+++ bot/utils/updateCounter/functions/removeChannelFromDB.js
@@ -6,7 +6,7 @@ module.exports = async ({ client, channelId, type, guildSettings, error, forceRe
         `[Bot shard #${client.shard.id}] I tried to update ${guildId}'s counter (channel: ${channelId}), but I don't have the proper permissions or it does not exists.${(error) ? "Error code:" + error.code : "" }`
     );
     //seems stupid to fetch again guildSettings but I'm trying to solve a parallel save error so creating a new instance from the scratch could fix it, if this comment lasts more than 30 days that will mean that I fixed it with this way
-    guildSettings = await GuildModel({ guild_id: guildId });
+    guildSettings = await GuildModel.findOne({ guild_id: guildId });
 
     if (forceRemove || error && (error.code === 50013 || error.code === 50001 || error.code === 10003 || error.code === 40001)) {
         switch (type) {
