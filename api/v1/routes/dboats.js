841c85034cb832cfc0ec0f9b91b12c709b5c20a9
eduardozgz <eduardozgz@protonmail.com>
2019-10-29 12:55:33 +0100

    Bug fixes and added stats post for discord.boats and discord.bots.gg
    Former-commit-id: 88aa53ae8a36e53a4cff1cf02322a84b6c93a9ee

---
+++ api/v1/routes/dboats.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const giveUpvoterRole = require("../../../bot/utils/giveUpvoterRole");
+const { DBOATS_WH_SECRET } = process.env;
+
+router.post("/dboats", (req, res) => {
+    const { authorization } = req.headers;
+    const webhook = req.body;
+    if (authorization === DBOATS_WH_SECRET) {
+        console.log(`[MAIN] [API] [DISCORD.BOATS] User ${webhook.user.id} upvoted the bot!`);
+        giveUpvoterRole(webhook.user.id, req.DiscordShardManager);
+        res.status(200);
+    } else res.status(401);
+});
+
+module.exports = router;
---
+++ bot/utils/giveUpvoterRole.js
@@ -0,0 +1,25 @@
+const {
+    DISCORD_OFFICIAL_SERVER_ID,
+    REWARD_ROLE_ID
+} = process.env;
+
+/**
+ * @param {string} user Discord user id
+ * @param {object} DiscordShardManager
+ */
+module.exports = (userId, DiscordShardManager) => {
+    DiscordShardManager.broadcastEval(`
+        const officialServer = this.guilds.get("${DISCORD_OFFICIAL_SERVER_ID}")
+        if (officialServer && officialServer.available && officialServer.members.get("${userId}")) {
+            officialServer
+                .members.get("${userId}")
+                    .addRole("${REWARD_ROLE_ID}")
+                        .then(() => {
+                            console.log("[MAIN] [API] Upvoter Role given successfully to ${userId}");
+                        })
+                        .catch(error => {
+                            console.error("[MAIN] [API] Error while trying to give the role to ${userId}, Error code: " + error.code);
+                        });
+        }
+    `);
+}
