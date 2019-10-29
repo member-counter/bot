7266ca08c8dc0d659c7bbe466a5ec467884dab03
eduardozgz <eduardozgz@protonmail.com>
2019-10-29 23:38:53 +0100

    Bug fixes
    Former-commit-id: dd25369c64facefdddd44773c3d0d9dbb24efde4

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -1,13 +1,17 @@
+const botStatsSender = require("../utils/botStatsSender");
+
 module.exports = client => {
     client.on("ready", () => {
         console.log(`[Bot shard #${client.shard.id}] Discord client ready`);
         console.log(
             `[Bot shard #${client.shard.id}] Serving on ${client.guilds.size} servers, for ${client.users.size} users as ${client.user.tag}`
         );
-
+        
+        botStatsSender(client);
         setPresence(client);
         
         setInterval(() => {
+            botStatsSender(client);
             setPresence(client);
         }, 15 * 60 * 1000);
     });
--- bot/utils/botStatsSender.js
+++ bot/utils/botStatsSender.js
@@ -1,52 +1,48 @@
-const INTERVAL_STATS_TIME = 15 * 60 * 1000;
 const { DISCORD_CLIENT_ID, DBL_TOKEN, DBGG_TOKEN, DBOATS_TOKEN } = process.env;
 const fetch = require("node-fetch");
 const getTotalGuildsAndMembers = require("./getTotalGuildsAndMembers");
 
-module.exports = client => {
+module.exports = async (client) => {
     if (client.shard.id === 0) {
-        setInterval(async () => {
-            const guildCount = await getTotalGuildsAndMembers(client).totalGuilds;
+        const guildCount = (await getTotalGuildsAndMembers(client)).totalGuilds;
 
-            //https://discord.bots.gg
-            fetch(`https://discord.bots.gg/api/v1/bots/${DISCORD_CLIENT_ID}/stats`, {
-                method: "post",
-                headers: {
-                    Authorization: DBGG_TOKEN,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ guildCount })
-            })
-                .then(response => response.json())
-                .then(console.log)
-                .catch(console.error);
+        //https://discord.bots.gg
+        fetch(`https://discord.bots.gg/api/v1/bots/${DISCORD_CLIENT_ID}/stats`, {
+            method: "post",
+            headers: {
+                Authorization: DBGG_TOKEN,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ guildCount })
+        })
+            .then(response => response.json())
+            .then(response => console.log(`[Bot shard #${client.shard.id}] [STATS SENDER] [discord.bots.gg] Stats sent, response: ${JSON.stringify(response)}`))
+            .catch(console.error);
 
-            //https://top.gg/
-            fetch(`https://top.gg/api/bots/${DISCORD_CLIENT_ID}/stats`, {
-                method: "post",
-                headers: {
-                    Authorization: DBL_TOKEN,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ server_count: guildCount })
-            })
-                .then(response => response.json())
-                .then(console.log)
-                .catch(console.error);
+        //https://top.gg/
+        fetch(`https://top.gg/api/bots/${DISCORD_CLIENT_ID}/stats`, {
+            method: "post",
+            headers: {
+                Authorization: DBL_TOKEN,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ server_count: guildCount })
+        })
+            .then(response => response.json())
+            .then(response => console.log(`[Bot shard #${client.shard.id}] [STATS SENDER] [top.gg] Stats sent, response: ${JSON.stringify(response)}`))
+            .catch(console.error);
 
-            //https://discord.boats/
-            fetch(`https://discord.boats/api/v2/bot/${DISCORD_CLIENT_ID}`, {
-                method: "post",
-                headers: {
-                    Authorization: DBOATS_TOKEN,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ server_count: guildCount })
-            })
-                .then(response => response.json())
-                .then(console.log)
-                .catch(console.error);
-
-        }, INTERVAL_STATS_TIME);
+        //https://discord.boats/
+        fetch(`https://discord.boats/api/v2/bot/${DISCORD_CLIENT_ID}`, {
+            method: "post",
+            headers: {
+                Authorization: DBOATS_TOKEN,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ server_count: guildCount })
+        })
+            .then(response => response.json())
+            .then(response => console.log(`[Bot shard #${client.shard.id}] [STATS SENDER] [discord.boats] Stats sent, response: ${JSON.stringify(response)}`))
+            .catch(console.error);
     }
 };
