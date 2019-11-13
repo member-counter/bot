f2106b24e32ed2c593df4ec2f6d5d9990a2b590b
eduardozgz <eduardozgz@protonmail.com>
2019-11-13 21:26:56 +0100

    Premium features (wip)
    Former-commit-id: c5ed73a980d2e583f905dae773e41b0384a0218d

--- mongooseModels/UserModel.js
+++ mongooseModels/UserModel.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user_id: { type: String, require: true }
+    user_id: { type: String, require: true },
+    premium_status: { type: Number, default: 0 }, //0 === no, 1 === low, 2 === high
 });
 
 module.exports = mongoose.model('users', UserSchema);
