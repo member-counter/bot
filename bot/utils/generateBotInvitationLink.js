57fc5db45c433fc6e7b6a208f35836af86c22355
eduardozgz <eduardozgz@protonmail.com>
2019-11-15 02:29:50 +0100

    Premium features done

--- bot/utils/generateBotInvitationLink.js
+++ bot/utils/generateBotInvitationLink.js
@@ -2,6 +2,6 @@ const { DISCORD_TOKEN, DISCORD_BOT_PERMISSIONS } = process.env;
 
 const base64botId = DISCORD_TOKEN.split('.').slice(0,1)[0];
 
-const botId = (new Buffer(base64botId)).toString('base64');
+const botId = Buffer.from(base64botId, 'base64').toString();
 
 module.exports = () => `https://discordapp.com/oauth2/authorize?client_id=${botId}&permissions=${DISCORD_BOT_PERMISSIONS}&scope=bot`;
