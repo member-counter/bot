2fced546096098fffc08403ade9b933a7de9b0f3
eduardozgz <30407412+eduardozgz@users.noreply.github.com>
2019-11-19 21:22:18 +0100

    Bug fix

--- bot/events/channelDelete.js
+++ bot/events/channelDelete.js
@@ -9,7 +9,7 @@ module.exports = client => {
                 .then(guild_settings => {
                     if (guild_settings) {
                         if (channel.type === "text" || channel.type === "news") {
-                            guild_settings.enabled_channels = guild_settings.enabled_channels.filter(channel_id => channel_id !== channel.id);
+                            guild_settings.topicCounterChannels.delete(channel.id)
                             guild_settings.save().catch(console.error);
                         } else if (channel.type === "voice") {
                             guild_settings.channelNameCounters.delete(channel.id);
