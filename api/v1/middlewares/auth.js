bd82da7bcf7701614d7675cd68b48cae7588db3c
eduardozgz <eduardozgz@protonmail.com>
2019-11-20 13:50:05 +0100

    api (wip)
    Former-commit-id: 1cbd2be2ce53ef4a38e05ee9663dd48f8b48c5f3

--- api/v1/middlewares/auth.js
+++ api/v1/middlewares/auth.js
@@ -2,27 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = (req, res, next) => {
     if (req.headers.authorization) {
-        fetch(`https://discordapp.com/api/v6/users/@me`, {
-            headers: { Authorization: req.headers.authorization }
-        })
-            .then(response => {
-                const { status } = response;
-                if (status === 200) {
-                    return response.json();
-                } else if (status === 401) {
-                    res.json({ code: 401, message: "Bad token" });
-                } else {
-                    res.json({ code: 500, message: "Backend error" });
-                }
-            })
-            .then(user => {
-                req.user = user;
-                next();
-            })
-            .catch(error => {
-                console.error(error);
-                res.json({ code: 500, message: "Backend error" });
-            });
+        //TODO check if the token has a valid signature
     } else {
         res.json({ code: 401, message: "Bad token" });
     }
--- api/v1/middlewares/isAdmin.js
+++ api/v1/middlewares/isAdmin.js
@@ -6,34 +6,6 @@ module.exports = (req, res, next) => {
     if (owners.includes(req.user.id)) {
         next();
     } else {
-        fetch(`https://discordapp.com/api/v6/users/@me/guilds`, {
-            headers: { Authorization: req.headers.authorization }
-        })
-            .then(response => {
-                const { status } = response;
-                if (status === 200) {
-                    return r.json();
-                } else if (status === 401) {
-                    res.json({ code: 401, message: "Bad token" });
-                } else {
-                    res.json({ code: 500, message: "Backend error" });
-                }
-            })
-            .then(guilds => {
-                const guilds_id = guilds.map(guild => guild = guild.id);
-                if (guilds_id.includes(req.params.id)) {
-                    guilds.forEach(guild => {
-                        if (guild.id === req.params.id) {
-                            const guildPermissions = new Discord.Permissions(guild.permissions);
-                            if (guildPermissions.has("ADMINISTRATOR")) next();
-                            else res.json({ code: 401, message: "Not authorized" });
-                        }
-                    });
-                } else res.json({ code: 401, message: "Not authorized" });
-            })
-            .catch(error => {
-                console.error(error);
-                res.json({ code: 500, message: "Backend error" });
-            });
+        //TODO check if use is admin in the guild (req.param.id)
     }
 };
