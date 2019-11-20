727395c7b587d4907d95919049b2cbb93a3f3884
eduardozgz <eduardozgz@protonmail.com>
2019-11-20 13:50:05 +0100

    api (wip)
    Former-commit-id: 1cbd2be2ce53ef4a38e05ee9663dd48f8b48c5f3

--- api/v1/routes/guild.js
+++ api/v1/routes/guild.js
@@ -4,18 +4,37 @@ const fetch = require("node-fetch");
 const auth = require("../middlewares/auth");
 const isAdmin = require("../middlewares/isAdmin");
 const TrackModel = require("../../../mongooseModels/TrackModel");
+const GuildModel = require("../../../mongooseModels/GuildModel");
+const UserModel = require("../../../mongooseModels/UserModel");
 
-//TODO
-//I should use streams
-router.get("/guilds/:id/member-count-history", auth, isAdmin, (req, res) => {
-    TrackModel.findOneAndUpdate({ guild_id: req.params.id }, {}, { upsert: true })
-        .then(result => {
-            res.json({ code: 0, count_history: result.count_history });
-        })
-        .catch(error => {
-            console.error(error);
-            res.json({ code: 500, message: "DB error" });
-        });
+//TODO get available guilds (check if the user has admin perms and if the bot is in the guild)
+router.get("/guilds", auth, (req, res) => {
+
+});
+
+//TODO get guild settings
+router.get("/guilds/settings", auth, isAdmin, (req, res) => {
+
+});
+
+//TODO patch guild settings
+router.patch("/guilds/:id/settings", auth, isAdmin, (req, res) => {
+
+});
+
+//TODO patch upgrade server tier
+router.patch("/guilds/:id/upgrade-server", auth, isAdmin, (req, res) => {
+
+});
+
+//TODO return available counts
+router.get("/guilds/:id/count-history", auth, isAdmin, (req, res) => {
+
+});
+
+//TODO use json streams
+router.get("/guilds/:id/count-history/:type", auth, isAdmin, (req, res) => {
+
 });
 
 module.exports = router;
