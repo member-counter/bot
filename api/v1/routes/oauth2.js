5a76ff22feb5d70052835617ee4f9e64bdfb9e4c
eduardozgz <eduardozgz@protonmail.com>
2019-11-21 14:22:45 +0100

    wip

--- api/v1/routes/oauth2.js
+++ api/v1/routes/oauth2.js
@@ -1,16 +1,14 @@
 const router = require("express").Router();
 const fetch = require("node-fetch");
-const owners = process.env.BOT_OWNERS.split(/,\s?/);
-const auth = require("../middlewares/auth");
 const jwt = require("jsonwebtoken");
-const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_OAUTH2_URL_REDIRECT, JWT_SECRET } = process.env;
+const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, DISCORD_OAUTH2_URI_REDIRECT, JWT_SECRET } = process.env;
 
 router.get("/oauth2", async (req, res) => {
     const accessCode = req.query.code;
     let data = new URLSearchParams();
     data.append("grant_type", "authorization_code");
     data.append("code", accessCode);
-    data.append("redirect_uri", DISCORD_OAUTH2_URL_REDIRECT);
+    data.append("redirect_uri", DISCORD_OAUTH2_URI_REDIRECT);
     data.append("scope", "identify");
 
     fetch(`https://discordapp.com/api/oauth2/token?${data}`, {
@@ -22,6 +20,7 @@ router.get("/oauth2", async (req, res) => {
     })
         .then(discordResponse => discordResponse.json())
         .then(discordResponse => {
+            console.log(discordResponse)
             fetch("https://discordapp.com/api/users/@me", {
                 headers: {
                     "Authorization": discordResponse.access_token
@@ -30,8 +29,9 @@ router.get("/oauth2", async (req, res) => {
                 .then(unparsedUser => unparsedUser.json())
                 .then(user => {
                     jwt.sign({ id: user.id }, JWT_SECRET, (err, token) => {
-                        if (err) throw new Error();
-                        res.json({ code: 0, token });
+                        if (err) throw err;
+                        console.log({ code: 0, token, discord_token: discordResponse.access_token })
+                        res.json({ code: 0, token, discord_token: discordResponse.access_token });
                     })
                 })
         })
@@ -39,10 +39,6 @@ router.get("/oauth2", async (req, res) => {
           console.error(error);
           res.json({ code: 500, message: "An error has occurred in the authorization process", error })
         });
-});
-
-router.get("/owner-check", auth, (req, res) => {
-    res.json({ code: 0, admin: owners.includes(req.user.id) });
 });
 
 module.exports = router;
