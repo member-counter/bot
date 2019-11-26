8d4f49db42dcf3d435bfb9e41f517a111b4402ad
eduardozgz <eduardozgz@protonmail.com>
2019-11-26 10:43:18 +0100

    banned members bug fixes (wip)

--- bot/utils/updateCounter/bannedMembers.js
+++ bot/utils/updateCounter/bannedMembers.js
@@ -1,4 +1,6 @@
 const setChannelName = require("./functions/setChannelName");
+const removeChannelFromDB = require("./functions/removeChannelFromDB");
+
 const addTrack = require("../addTrack");
 
 module.exports = async (client, guildSettings) => {
@@ -8,11 +10,32 @@ module.exports = async (client, guildSettings) => {
     } = guildSettings;
     
     if (client.guilds.has(guild_id) && client.guilds.get(guild_id).available) {
-        const count = (await client.guilds.get(guild_id).fetchBans()).size;
-        channelNameCounters.forEach((channelNameCounter, channelId) => {
-            if (channelNameCounter.type === "bannedmembers")
-                setChannelName({ client, channelId, channelName: channelNameCounter.channelName, count, guildSettings });
-        });
-        addTrack(guild_id, "banned_member_count_history", count);
+        client.guilds.get(guild_id)
+            .fetchBans()
+                .then(bans => {
+                    const count = bans.size;
+                    channelNameCounters.forEach((channelNameCounter, channelId) => {
+                        if (channelNameCounter.type === "bannedmembers")
+                            setChannelName({ client, channelId, channelName: channelNameCounter.channelName, count, guildSettings });
+                    });
+                    addTrack(guild_id, "banned_member_count_history", count);
+                })
+                .catch(error => {        
+                    channelNameCounters.forEach((channelNameCounter, channelId) => {
+                        if (channelNameCounter.type === "bannedmembers") {
+                            client.channels.get(channelId)
+                                .delete()
+                                    .catch(console.error);
+
+                            removeChannelFromDB({
+                                client,
+                                guildSettings,
+                                channelId,
+                                type: "channelNameCounter",
+                                error
+                            });
+                        }
+                    });
+                });
     }
 };
