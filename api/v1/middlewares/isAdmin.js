2232721fa7330a077cf60470bf9f36ba75bd510f
eduardozgz <eduardozgz@protonmail.com>
2019-12-05 02:51:45 +0100

    API: Get guilds and generate tokens easily for dev

--- api/v1/middlewares/isAdmin.js
+++ api/v1/middlewares/isAdmin.js
@@ -1,28 +1,38 @@
 const owners = process.env.BOT_OWNERS.split(/,\s?/);
 const GuildModel = require("../../../mongooseModels/GuildModel");
 
-//TODO test this
 module.exports = (req, res, next) => {
     if (owners.includes(req.user.id)) {
         next();
     } else {
         GuildModel.findOneAndUpdate({ guild_id: req.params.guildId }, {}, { upsert: true, projection: { allowedRoles: 1 } })
             .then(guildSettings => {
-                req.DiscordShardManager.broadcastEval(`
-                    this.guild.get(${req.params.guildId})
-                        .members.get(${req.user.id})
-                            .permissions.has("ADMINISTRATOR")
-                    ||
-                    this.guild.get(${req.params.guildId})
-                        .members.get(${req.user.id})
-                            .roles.some(role => ${JSON.stringify(guildSettings.allowedRoles)}.includes(role.id))
-                `)
+                const condition = `
+                    (() => {
+                        const guildId = "${guild.id}",
+                            userId = "${req.user.id}",
+                            allowedRoles = ${JSON.stringify(allowedRoles)}; 
+                        if (this.guilds.has(guildId) && this.guilds.get(guildId).members.has(userId)) {
+                            const member = this.guilds.get(guildId).members.get(userId);
+                            return (
+                                member.permissions.has("ADMINISTRATOR")
+                                ||
+                                member.roles.some(role => allowedRoles.includes(role.id))
+                            );
+                        } else {
+                            return false;
+                        }
+                    })();
+                `;
+
+                req.DiscordShardManager.broadcastEval(condition)
                     .then(results => {
                         if (results.includes(true)) next();
+                        else { res.status(403).json({ error: "You are not authorized to perform actions on this discord server" }); }
                     });
             })
             .catch(error => {
-                res.send({ code: 500, error: "Internal server error" });
+                res.status(500).json({ error: "Internal server error" });
                 console.error(error);
             }); 
     }
