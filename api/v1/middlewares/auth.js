e8897d4fa6953c74603fb4f6546c8a0749a5becc
eduardozgz <eduardozgz@protonmail.com>
2019-12-06 18:50:18 +0100

    Skip test

--- api/v1/middlewares/auth.js
+++ api/v1/middlewares/auth.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
     jwt.verify(req.headers.authorization, JWT_SECRET, (err, decoded) => {
-        if (err) res.json({ code: 401, message: "Bad token" });
+        if (err) res.status(401).json({ message: "Bad token" });
         else {
             req.token = decoded;
             next();
