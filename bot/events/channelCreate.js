ad6507f61f5e18c9f771422a34c73e0a46f09bb2
eduardozgz <eduardozgz@protonmail.com>
2020-01-06 01:37:58 +0100

    Bug fixed - Bot not responding to DMs

--- bot/events/channelCreate.js
+++ bot/events/channelCreate.js
@@ -3,7 +3,9 @@ const guildCounts = require("../utils/guildCounts");
 
 module.exports = client => {
     client.on("channelCreate", channel => {
-        if (channel.type !== "category") guildCounts.get(channel.guild.id).increment("channels", 1);
-        if (channel.guild) updateCounter({client, guildSettings: channel.guild.id});
+        if (channel.guild) {
+            if (channel.type !== "category") guildCounts.get(channel.guild.id).increment("channels", 1);
+            updateCounter({client, guildSettings: channel.guild.id});
+        }
     });
 };
