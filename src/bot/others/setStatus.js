4f00c6a5a2252c8d8f82d7dac238739f8cdcf01e
eduardozgz <eduardozgz@protonmail.com>
2020-01-19 00:53:27 +0100

    wip

---
+++ src/bot/others/setStatus.js
@@ -0,0 +1,8 @@
+const { DISCORD_PREFIX } = process.env;
+
+module.exports = (client) => {
+    // client.editStatus("online", {
+    //     name: `${process.env.DISCORD_PREFIX}help`,
+    //     type: 3
+    // });
+}
