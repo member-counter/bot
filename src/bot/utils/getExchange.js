6f7a7d098bc38d3ebd5a631a4bd4bf7195bf2f41
eduardozgz <eduardozgz@protonmail.com>
2020-03-31 17:33:24 +0200

    Improved response time in mc!donate

--- src/bot/utils/getExchange.js
+++ src/bot/utils/getExchange.js
@@ -1,18 +1,17 @@
 const fetch = require("node-fetch");
 
-const exchangeDiscardCacheTime = 12 * 60 * 60 * 1000; //12 hours
-let nextExchangeFetch = new Date();
-    nextExchangeFetch.setMilliseconds(exchangeDiscardCacheTime);
 let exchangeCache = null;
+
+setInterval(() => {
+    exchangeCache = null;
+}, 24 * 60 * 60 * 1000);
 
 module.exports = () => {
     return new Promise((resolve, reject) => {
-        if (!exchangeCache || (new Date()).getTime() > nextExchangeFetch.getTime()) {
+        if (!exchangeCache) {
             fetch(`https://api.exchangeratesapi.io/latest`)
                 .then(res => res.json())
                 .then(exchange => {
-                    nextExchangeFetch = new Date();
-                    nextExchangeFetch.setMilliseconds(exchangeDiscardCacheTime);
                     exchangeCache = exchange;
                     resolve(exchange);
                 })
