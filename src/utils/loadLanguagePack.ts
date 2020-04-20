5f28a8bd1a10bd5592b60d965ab7947487f539b7
eduardozgz <eduardozgz@protonmail.com>
2020-04-21 00:18:03 +0200

    Russian language added

--- src/utils/loadLanguagePack.ts
+++ src/utils/loadLanguagePack.ts
@@ -3,11 +3,13 @@ import clonedeep from 'lodash.clonedeep';
 import * as en_US from '../lang/en_US.json';
 import * as es_ES from '../lang/es_ES.json';
 import * as pt_BR from '../lang/pt_BR.json';
+import * as ru_RU from '../lang/ru_RU.json';
 
 const languagePacks: object = {
   en_US,
   es_ES,
   pt_BR,
+  ru_RU,
 };
 
 export default (langCode: string): any => {
