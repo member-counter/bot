649f121a9f6c0e292c25dba33cfca68696346e00
eduardozgz <eduardozgz@protonmail.com>
2020-05-25 03:23:57 +0200

    channelUrl -> username

--- src/counts/externalCounts/Mixer.ts
+++ src/counts/externalCounts/Mixer.ts
@@ -6,10 +6,10 @@ interface stats {
   followers: number;
 }
 
-const getChannelStats = async (channelUrl: string): Promise<stats> => {
+const getChannelStats = async (username: string): Promise<stats> => {
 
   const response = await fetch(
-    `https://mixer.com/api/v1/channels/${channelUrl}`
+    `https://mixer.com/api/v1/channels/${username}`
   ).then((response) => response.json());
 
   const followers = response?.numFollowers;
