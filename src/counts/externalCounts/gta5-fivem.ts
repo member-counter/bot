db3201a4556880e6c665d345072e3c71bfda6252
eduardozgz <eduardozgz@protonmail.com>
2020-06-27 15:05:16 +0200

    More bug fixes

--- src/counts/externalCounts/gta5-fivem.ts
+++ src/counts/externalCounts/gta5-fivem.ts
@@ -1,31 +1,22 @@
 import fetch from 'node-fetch';
 import AbortController from 'abort-controller';
 import * as packageJSON from '../../../package.json';
+import timeoutFetch from '../../utils/timeoutFetch';
 
-export default (resource: string): Promise<number> =>
-	new Promise(async (resolve, reject) => {
-		let count: number = 0;
-
+export default async (resource: string): Promise<number> => {
 		const controller = new AbortController();
-		setTimeout(() => {
-			controller.abort();
-			reject(new Error(`[FIVEM] HTTP request time out: ${resource}`));
-		}, 10 * 1000);
-
-		const response = await fetch(`http://${resource}/players.json`, {
+		const response = await  timeoutFetch(10000, fetch(`http://${resource}/players.json`, {
 			signal: controller.signal,
 			headers: {
 				'User-Agent': `Member Counter Discord Bot/${packageJSON.version}`,
 			},
-		});
+		}), controller);
 
 		if (response.status === 200) {
 			const result = await response.json();
-			count = result?.length;
+			return result?.length;
 		} else {
       controller.abort();
-			reject(new Error(`[FIVEM] Invalid status code (not 200) in: ${resource}`));
+			throw new Error(`[FIVEM] Invalid status code (not 200) in: ${resource}`);
 		}
-
-		resolve(count);
-	});
+};
--- src/counts/externalCounts/minecraft.ts
+++ src/counts/externalCounts/minecraft.ts
@@ -1,31 +1,24 @@
 import fetch from 'node-fetch';
 import AbortController from 'abort-controller';
 import * as packageJSON from '../../../package.json';
+import timeoutFetch from '../../utils/timeoutFetch';
 
-export default async (resource: string): Promise<number> =>
-new Promise(async (resolve, reject) => {
-  let count: number = 0;
-
+export default async (resource: string): Promise<number> => {
   const controller = new AbortController();
-  const timeout = setTimeout(() => {
-    controller.abort();
-    reject(new Error(`[MINECRAFT] HTTP request time out: ${resource}`));
-  }, 10 * 1000);
-
-  const response = await fetch(`https://api.mcsrvstat.us/2/${resource}`, {
+  const response = await timeoutFetch(10000, fetch(`https://api.mcsrvstat.us/2/${resource}`, {
     signal: controller.signal,
     headers: {
       'User-Agent': `Member Counter Discord Bot/${packageJSON.version}`,
     },
-  });
+  }), controller);
 
+  console.log(response.status)
   if (response.status === 200) {
     const result = await response.json();
-    count = result?.players?.online;
+    console.log(result)
+    return result?.players?.online;
   } else {
     controller.abort();
-    reject(new Error(`[MINECRAFT] Invalid status code (not 200) in: ${resource}`));
-  }
-
-  resolve(count);
-});
+    throw new Error(`[MINECRAFT] Invalid status code (not 200) in: ${resource}`);
+  } 
+}
