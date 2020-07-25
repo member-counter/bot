ac07e1fb349df30351bf456e9bc28d0793f93160
eduardozgz <eduardozgz@protonmail.com>
2020-07-25 15:55:03 +0200

    round to nearest 5min

--- src/counters/clock.ts
+++ src/counters/clock.ts
@@ -6,11 +6,17 @@ const ClockCounter: Counter = {
 	isEnabled: true,
 	lifetime: 0,
 	execute: async ({ guildSettings, resource: timeZone }) => {
+		// TODO website to select timezone https://member-counter.eduardozgz.com/utils/clock
+		const coeff = 1000 * 60 * 5;
+		const date = new Date();
+		const rounded = new Date(Math.round(date.getTime() / coeff) * coeff)
+
+
 		return new Intl.DateTimeFormat(guildSettings.locale, {
 			hour: 'numeric',
 			minute: 'numeric',
 			timeZone,
-		}).format();
+		}).format(rounded);
 	},
 };
 
