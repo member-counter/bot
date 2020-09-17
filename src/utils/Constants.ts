134fbfc5f861d84ff78199d6fad4340269b7a810
eduardozgz <eduardozgz@protonmail.com>
2020-09-17 10:00:32 +0000

    refactor

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,12 +1,25 @@
-const CounterResult = {
-	PREMIUM: -1,
-	ERROR: -2,
-	UNKNOWN: -3,
-	DISABLED: -4,
+enum UserBadges {
+	DONATOR = 1,
+	SPONSOR = 1 << 1,
+	BETA_TESTER = 1 << 2,
+	TRANSLATOR = 1 << 3,
+	CONTRIBUTOR = 1 << 4,
+	BIG_BRAIN = 1 << 5,
+	BUG_CATCHER = 1 << 6,
+	PATPAT = 1 << 7,
+}
+
+enum CounterResult {
+	PREMIUM = -1,
+	ERROR = -2,
+	UNKNOWN = -3,
+	DISABLED = -4,
 }
 
 const Constants = {
 	CounterResult,
+	UserBadges,
 };
 
 export default Constants;
+export { Constants, UserBadges };
