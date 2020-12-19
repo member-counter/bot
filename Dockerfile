6523fc6090f624b00bc56ad67b6b0d57a2f89c79
eduardozgz <eduardozgz@protonmail.com>
2020-12-19 16:51:53 +0100

    dominant avatar color as embed color and gracefull shutdown

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN npm install
 
 RUN npm run build
 
-CMD ["npm", "start"]
+CMD ["node", "."]
--- src/utils/embedBase.ts
+++ src/utils/embedBase.ts
@@ -1,16 +1,26 @@
 import { EmbedOptions } from 'eris';
-import getEnv from './getEnv';
+import Bot from '../bot';
+import ColorThief from 'colorthief';
 
-const { PREMIUM_BOT } = getEnv();
+let dominantColor = null;
 
-const embedBase = (embedToAppend?: EmbedOptions): EmbedOptions => ({
-  color: PREMIUM_BOT ? 16723200 : 14503424,
-  footer: {
-    icon_url:
-      'https://cdn.discordapp.com/avatars/343884247263608832/98ce0df05fc35de2510c045cb469e4f7.png?size=64',
-    text: 'by eduardozgz#5695',
-  },
-  ...embedToAppend,
-});
+const embedBase = (embedToAppend?: EmbedOptions): EmbedOptions => {
+  if (!dominantColor) {
+    ColorThief.getColor(Bot.client.user.dynamicAvatarURL("png", 1024))
+      .then((colors: [number, number, number]) => {
+        dominantColor = colors[0] << 16 | colors[1] << 8 | colors[2];
+      });
+  }
+
+  return {
+    color: dominantColor,
+    footer: {
+      icon_url:
+        'https://cdn.discordapp.com/avatars/343884247263608832/98ce0df05fc35de2510c045cb469e4f7.png?size=64',
+      text: 'by eduardozgz#5695',
+    },
+    ...embedToAppend,
+  }
+};
 
 export default embedBase;
