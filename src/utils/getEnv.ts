a1cf0659f7dcdf2d781005b01efd1ef17661ed41
eduardozgz <eduardozgz@protonmail.com>
2021-01-07 20:35:46 +0100

    Update getEnv.ts

--- src/utils/getEnv.ts
+++ src/utils/getEnv.ts
@@ -74,7 +74,7 @@ const parsedEnv = dotenvParseVariables({
 	...process.env,
 	DISCORD_CLIENT_ID: Buffer.from(process.env.DISCORD_CLIENT_TOKEN.split(".")[0], 'base64').toString("utf-8") + "*",
 	// Since k8s doesn't offer any clean way to get the id of a pod in a statefulset, we must extract it from the pod's name
-	FIRST_SHARD: Number((process.env.FIRST_SHARD.match(/(\d+)/)[0])),
+	FIRST_SHARD: Number((process.env.FIRST_SHARD?.match(/(\d+)/)[0])),
 });
 
 function getEnv(): MemberCounterEnv {
