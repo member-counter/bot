cbad5aea5fa8f5f0658740a35fc27c66e149efde
eduardozgz <eduardozgz@protonmail.com>
2021-01-15 18:43:20 +0100

    bug fix

--- src/jobs/fetchMemberCounts.ts
+++ src/jobs/fetchMemberCounts.ts
@@ -4,14 +4,19 @@ import Job from "../typings/Job";
 import getEnv from "../utils/getEnv";
 import sleep from "../utils/sleep";
 
-const { MEMBER_COUNTS_CACHE_CHECK_SLEEP, MEMBER_COUNTS_CACHE_LIFETIME } = getEnv();
+const { MEMBER_COUNTS_CACHE_CHECK_SLEEP, MEMBER_COUNTS_CACHE_LIFETIME, PREMIUM_BOT } = getEnv();
 
 const fetchMemberCounts: Job = {
   time: '0 */10 * * * *',
   runAtStartup: true,
   runInOnlyFirstThread: true,
   run: async ({ client }) => {
-    const guilds = (await client.evalAll('[...this.client.guilds.keys()]') as string[]).flat();
+    /**
+     * The main and the premium bot are "connected" throught redis,
+     * the premium bot tries to fetch guilds of the main bot which doesn't have access, and it's useless,
+     * since the premium bot runs in only one proccess, we can obtain the guild list directly
+     */
+    const guilds = PREMIUM_BOT ? (client.guilds.keys() as unknown as string[]) : ((await client.evalAll('[...this.client.guilds.keys()]')) as string[]).flat();
 
     for (const id of guilds) {
       try {
