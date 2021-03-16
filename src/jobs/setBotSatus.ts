e0b9df147cbc5386339a7d6992825f5865b64050
eduardozgz <eduardozgz@protonmail.com>
2021-03-16 13:57:13 +0100

    Random status, and a command to set the status

--- src/jobs/setBotSatus.ts
+++ src/jobs/setBotSatus.ts
@@ -1,17 +1,22 @@
+import setStatus from "../others/setStatus";
 import Job from "../typings/Job";
 import getEnv from "../utils/getEnv";
 
-const { DISCORD_PREFIX } = getEnv();
+const { DISCORD_STATUS } = getEnv();
 
 const setBotStatus: Job = {
 	time: "0 */5 * * * *",
 	runAtStartup: true,
 	runInOnlyFirstThread: false,
 	run: async ({ client }) => {
-		client.editStatus("online", {
-			name: `${DISCORD_PREFIX}help`,
-			type: 3
-		});
+		let statusToSet = DISCORD_STATUS[0] ?? "{online;watching} {prefix}help";
+
+		if (Math.random() * 100 < 1) {
+			statusToSet =
+				DISCORD_STATUS[Math.floor(Math.random() * DISCORD_STATUS.length)];
+		}
+
+		setStatus(client, statusToSet);
 	}
 };
 
