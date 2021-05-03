dcb71f2681ec380f9717f2d8a1d1cf686c9e49cc
VampireChicken12 <Johnbrown51@yahoo.com>
2021-05-03 18:19:26 -0400

    Add reddit subreddit member counter support

--- src/counters/all.ts
+++ src/counters/all.ts
@@ -15,6 +15,7 @@ import MembersExtendedCounter from "./membersExt";
 import MembersWithRoleCounter from "./membersWithRole";
 import MemeratorCounter from "./Memerator";
 import NitroBoostersCounter from "./nitroBoosters";
+import redditMembers from "./redditMembers";
 import RolesCounter from "./roles";
 import StaticCounter from "./static";
 import TwitchCounter from "./Twitch";
@@ -40,6 +41,7 @@ const counters = [
 	MembersWithRoleCounter,
 	MemeratorCounter,
 	NitroBoostersCounter,
+	redditMembers,
 	RolesCounter,
 	StaticCounter,
 	TwitchCounter,
