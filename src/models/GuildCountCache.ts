d7474bbb7f4fdc93355bfd0ed111e03ef7fd2f35
Eduardo <eduardozgz@protonmail.com>
2021-08-04 23:02:29 +0200

    Bug fixes

--- src/models/GuildCountCache.ts
+++ src/models/GuildCountCache.ts
@@ -1,14 +1,14 @@
 import { model, Schema, Document } from "mongoose";
 
 interface GuildCountCacheDocument extends Document {
-	guild: string;
+	id: string;
 	members: number;
 	onlineMembers: number;
 	timestamp: number;
 }
 
 const GuildCountCacheSchema = new Schema({
-	guild: { type: String, require: true },
+	id: { type: String, require: true },
 	members: { type: Number, require: true },
 	onlineMembers: { type: Number, require: true },
 	timestamp: { type: Number, require: true, default: Date.now }
