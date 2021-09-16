85444596e59663b086d97fb6e50449e6c023afd9
Eduardo <eduardozgz@protonmail.com>
2021-09-16 15:07:14 +0200

    Add support for multiple games in membersPlaying

--- src/counters/membersPlaying.ts
+++ src/counters/membersPlaying.ts
@@ -5,14 +5,16 @@ const MembersPlayingCounter: Counter = {
 	isPremium: true,
 	isEnabled: true,
 	lifetime: 0,
-	execute: async ({ client, guild, unparsedArgs: resource }) => {
+	execute: async ({ client, guild, unparsedArgs }) => {
 		let count = 0;
+
+		const games = unparsedArgs.split(",").map((game) => game.trim().toLowerCase());
 
 		for (const [memberId, member] of guild.members) {
 			if (
 				!member.bot &&
-				member.game &&
-				member.game.name.toLowerCase() === resource.toLowerCase()
+				member.game?.name &&
+				games.some((game) => game === member.game.name.trim().toLowerCase())
 			) {
 				count++;
 			}
