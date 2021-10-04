781988037d0839f7b08af9a8c4c7b5353e1b0e4d
Eduardo <eduardozgz@protonmail.com>
2021-10-04 17:28:28 +0200

    bug fix

--- src/utils/getBotInviteLink.ts
+++ src/utils/getBotInviteLink.ts
@@ -6,7 +6,7 @@ const { DISCORD_CLIENT_ID } = getEnv();
 const inviteLink = new URL('https://discord.com/oauth2/authorize');
 inviteLink.searchParams.set('client_id', DISCORD_CLIENT_ID);
 inviteLink.searchParams.set('permissions', neededBotPermissionsBitField.toString());
-inviteLink.searchParams.set('scope', ['bot', 'applications.commands'].join());
+inviteLink.searchParams.set('scope', ['bot', 'applications.commands'].join(" "));
 
 function getBotInviteLink(): string {
 	return inviteLink.toString();
