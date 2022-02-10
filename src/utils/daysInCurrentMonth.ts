362ca9937d4ed6a9c2ff6bf17d7f6f8f67b0e959
Eduardo <eduardozgz@protonmail.com>
2022-02-10 22:52:08 +0100

    Revert "add commandStats command etc"
    This reverts commit cb077b9623b00b96077cea25894f445915a801fa.

--- src/utils/daysInCurrentMonth.ts
+++
@@ -1,6 +0,0 @@
-function daysInThisMonth(): number {
-	var now: Date = new Date();
-	return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-}
-const daysInCurrentMonth = daysInThisMonth();
-export default daysInCurrentMonth;
