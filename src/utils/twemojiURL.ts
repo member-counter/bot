bd437f33abb0a137faac38865c005814aab643f1
Eduardo Aznar <eduardozgz@protonmail.com>
2022-09-11 02:28:20 +0200

    fix: build

--- src/utils/twemojiURL.ts
+++ src/utils/twemojiURL.ts
@@ -1,5 +1,7 @@
 import twemoji from "twemoji";
 
 export function twemojiURL(emoji: string): string {
-	return `${twemoji.base}72x72/${twemoji.convert.toCodePoint(emoji)}.png`;
+	return `https://twemoji.maxcdn.com/v/14.0.2/72x72/${twemoji.convert.toCodePoint(
+		emoji
+	)}.png`;
 }
