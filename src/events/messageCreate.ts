3dfd6f16f8654e1aac12bbc439e425b73ea6b256
Eduardo Aznar <eduardozgz@protonmail.com>
2022-09-20 16:28:05 +0200

    simplify the huevo

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,25 +7,23 @@ import { Event } from "../structures";
 export const messageCreateEvent = new Event({
 	name: "messageCreate",
 	handler: async (message: Message) => {
-		if (message.author.bot) return;
-		const command = message.content
-			.replace(message.client.user.toString() + " ", "")
-			.replace(
-				message.guild.members.cache
-					.get(message.client.user.id)
-					.roles.botRole?.toString() + " ",
-				""
-			);
-		if (command !== "patpat") {
-			return;
-		}
-		const userSettings = await UserService.init(message.author.id);
-		await userSettings.grantBadge(Constants.UserBadges.PATPAT);
+		{
+			if (message.author.bot) return;
+			const [, command] = message.content.split(/s+/);
 
-		await message.channel.send(
-			"https://eduardozgz.com/max?discord_cache_fix=" + Math.random()
-		);
-		// i never thought i will ever been coding with a very cute guy -alex
+			if (command !== "patpat") {
+				return;
+			}
+
+			const userSettings = await UserService.init(message.author.id);
+			await userSettings.grantBadge(Constants.UserBadges.PATPAT);
+
+			// Huevo de pascua:
+			await message.channel.send(
+				"https://eduardozgz.com/max?discord_cache_fix=" + Math.random()
+			);
+			// i never thought i will ever been coding with a very cute guy -alex
+		}
 	},
 	neededIntents: ["GuildMessages"]
 });
