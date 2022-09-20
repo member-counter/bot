d63291e5e42c920f78b8710cdfc717fa67cec0b3
VampireChicken12 <Johnbrown51@yahoo.com>
2022-09-20 14:22:02 -0400

    refactor: Remove wrapping curly braces

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -9,41 +9,36 @@ import { Event } from "../structures";
 export const guildCreateEvent = new Event({
 	name: "guildCreate",
 	handler: async (guild: Guild) => {
-		{
-			const {
-				premium: {
-					thisBotsIsPremium: PREMIUM_BOT,
-					premiumBotId: PREMIUM_BOT_ID
-				},
-				unrestrictedMode: UNRESTRICTED_MODE
-			} = config;
-			const guildSettings = await GuildSettings.init(guild.id);
+		const {
+			premium: { thisBotsIsPremium: PREMIUM_BOT, premiumBotId: PREMIUM_BOT_ID },
+			unrestrictedMode: UNRESTRICTED_MODE
+		} = config;
+		const guildSettings = await GuildSettings.init(guild.id);
 
-			if (guildSettings.blocked) {
-				await guild.leave();
-				return;
+		if (guildSettings.blocked) {
+			await guild.leave();
+			return;
+		}
+
+		await guildSettings.setLocale(guild.preferredLocale);
+
+		// Self kick when premium bot is present and the guild is premium
+		if (!PREMIUM_BOT && !UNRESTRICTED_MODE) {
+			const premiumBotMember = guild.members.cache.get(PREMIUM_BOT_ID);
+
+			if (guildSettings.premium && premiumBotMember) {
+				guild.leave().catch(logger.error);
 			}
+		}
 
-			await guildSettings.setLocale(guild.preferredLocale);
-
-			// Self kick when premium bot is present and the guild is premium
-			if (!PREMIUM_BOT && !UNRESTRICTED_MODE) {
-				const premiumBotMember = guild.members.cache.get(PREMIUM_BOT_ID);
-
-				if (guildSettings.premium && premiumBotMember) {
-					guild.leave().catch(logger.error);
-				}
-			}
-
-			// set language
-			const languageToSet = guild.preferredLocale.replace(/-/g, "_");
-			if (
-				availableLocales.includes(
-					languageToSet as typeof availableLocales[number]
-				)
-			) {
-				await guildSettings.setLanguage(languageToSet);
-			}
+		// set language
+		const languageToSet = guild.preferredLocale.replace(/-/g, "_");
+		if (
+			availableLocales.includes(
+				languageToSet as typeof availableLocales[number]
+			)
+		) {
+			await guildSettings.setLanguage(languageToSet);
 		}
 	},
 	neededIntents: ["Guilds"]
