86825f35bb1d257b90087cd9e6685c02a7f4fc62
VampireChicken12 <Johnbrown51@yahoo.com>
2022-09-20 09:31:33 -0400

    feat: Added mentionable patpat command close #342

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,8 +2,13 @@ import { IntentsBitField, PermissionsBitField } from "discord.js";
 import type { Event } from "../structures";
 import { interactionCreateEvent } from "./interactionCreate";
 import { readyEvent } from "./ready";
+import { messageCreateEvent } from "./messageCreate";
 
-const allEvents: Event<any>[] = [readyEvent, interactionCreateEvent];
+const allEvents: Event<any>[] = [
+	readyEvent,
+	interactionCreateEvent,
+	messageCreateEvent
+];
 
 const allEventsNeededPermissions: PermissionsBitField = new PermissionsBitField(
 	allEvents.map((e) => e.neededPermissions)
