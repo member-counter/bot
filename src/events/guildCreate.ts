b7acdd122ad983a2efb52505aa6feac199e70fb7
VampireChicken12 <Johnbrown51@yahoo.com>
2022-09-21 07:36:07 -0400

    refactor: Fix TODOs

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -8,10 +8,9 @@ import { Event } from "../structures";
 export const guildCreateEvent = new Event({
 	name: "guildCreate",
 	handler: async (guild: Guild) => {
-		// TODO: use names directly
 		const {
-			premium: { thisBotsIsPremium: PREMIUM_BOT, premiumBotId: PREMIUM_BOT_ID },
-			unrestrictedMode: UNRESTRICTED_MODE
+			premium: { thisBotsIsPremium, premiumBotId },
+			unrestrictedMode
 		} = config;
 		const guildSettings = await GuildSettings.init(guild.id);
 
@@ -23,9 +22,8 @@ export const guildCreateEvent = new Event({
 		await guildSettings.setLocale(guild.preferredLocale);
 
 		// Self kick when premium bot is present and the guild is premium
-		if (!PREMIUM_BOT && !UNRESTRICTED_MODE) {
-			// TODO: fetch because it wont be in the cache
-			const premiumBotMember = guild.members.cache.get(PREMIUM_BOT_ID);
+		if (!thisBotsIsPremium && !unrestrictedMode) {
+			const premiumBotMember = await guild.members.fetch(premiumBotId);
 
 			if (guildSettings.premium && premiumBotMember) {
 				guild.leave().catch(logger.error);
