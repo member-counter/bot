b14a87da5f8ce9e295afba13fabdf42a3ae97900
VampireChicken12 <Johnbrown51@yahoo.com>
2022-09-20 23:48:11 -0400

    refactor: Remove wrapping curly braces and fix patpat

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,23 +7,21 @@ import { Event } from "../structures";
 export const messageCreateEvent = new Event({
 	name: "messageCreate",
 	handler: async (message: Message) => {
-		{
-			if (message.author.bot) return;
-			const [, command] = message.content.split(/s+/);
+		if (message.author.bot) return;
+		const [, command] = message.content.split(/ +/);
 
-			if (command !== "patpat") {
-				return;
-			}
-
-			const userSettings = await UserService.init(message.author.id);
-			await userSettings.grantBadge(Constants.UserBadges.PATPAT);
-
-			// Huevo de pascua:
-			await message.channel.send(
-				"https://eduardozgz.com/max?discord_cache_fix=" + Math.random()
-			);
-			// i never thought i will ever been coding with a very cute guy -alex
+		if (command !== "patpat") {
+			return;
 		}
+
+		const userSettings = await UserService.init(message.author.id);
+		await userSettings.grantBadge(Constants.UserBadges.PATPAT);
+
+		// Huevo de pascua:
+		await message.channel.send(
+			"https://eduardozgz.com/max?discord_cache_fix=" + Math.random()
+		);
+		// i never thought i will ever been coding with a very cute guy -alex
 	},
 	neededIntents: ["GuildMessages"]
 });
