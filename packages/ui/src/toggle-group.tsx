4eced15b55aee1b6e9b325888970edd0369068fe
eduardozgz <eduardozgz@protonmail.com>
2024-05-10 13:04:43 +0200

    fix eslint

--- packages/ui/src/toggle-group.tsx
+++ packages/ui/src/toggle-group.tsx
@@ -44,8 +44,8 @@ const ToggleGroupItem = React.forwardRef<
       ref={ref}
       className={cn(
         toggleVariants({
-          variant: context.variant || variant,
-          size: context.size || size,
+          variant: context.variant ?? variant,
+          size: context.size ?? size,
         }),
         className,
       )}
