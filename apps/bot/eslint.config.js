c4724dc8e0425c9e08859d535cb81e99a7c7dbd4
eduardozgz <eduardozgz@protonmail.com>
2024-05-12 22:31:49 +0200

    fix lint

--- apps/bot/eslint.config.js
+++ apps/bot/eslint.config.js
@@ -3,7 +3,7 @@ import baseConfig from "@mc/eslint-config/base";
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ["dist/**"],
   },
   ...baseConfig,
 ];
