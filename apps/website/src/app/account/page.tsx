ec81260cc1c0dbac5eb6298e87a38fa955c54e01
eduardozgz <eduardozgz@protonmail.com>
2024-05-12 22:33:27 +0200

    linting

--- apps/website/src/app/account/page.tsx
+++ apps/website/src/app/account/page.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 
-import { api } from "~/trpc/server";
-
 export const metadata: Metadata = { title: "Account - Member Counter" };
 export default async function Page() {
   // TODO
