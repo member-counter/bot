b9388ae99013ccd1285be6afc5c5573c8b8a14e2
eduardozgz <eduardozgz@protonmail.com>
2024-05-18 18:53:30 +0000

    bug fix

--- apps/website/src/app/dashboard/servers/[guildId]/layout.tsx
+++ apps/website/src/app/dashboard/servers/[guildId]/layout.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import { useContext } from "react";
-
-import { api } from "~/trpc/react";
-import { BlockedBanner } from "./BlockedBanner";
-import { ForbiddenPage } from "./ForbiddenPage";
-import { InviteBotBanner } from "./InviteBotBanner";
-import { InviteBotPage } from "./InviteBotPage";
-import { ManageServer } from "./ManageServer";
-import {
-  UserPermissionsContext,
-  UserPermissionsContextProvider,
-} from "./UserPermissionsContext";
+import LayoutInner from "./layoutInner";
+import { UserPermissionsContextProvider } from "./UserPermissionsContext";
 
 export interface DashboardGuildParams {
   guildId: string;
@@ -23,37 +13,10 @@ export interface DashboardGuildPageProps {
   params: DashboardGuildParams;
 }
 
-export default function Layout({
-  children,
-  params: { guildId },
-}: DashboardGuildPageProps) {
-  const userPermissions = useContext(UserPermissionsContext);
-
-  const has = api.guild.has.useQuery({
-    discordGuildId: guildId,
-  });
-
-  if (!has.isSuccess) return;
-
+export default function Layout({ children }: DashboardGuildPageProps) {
   return (
     <UserPermissionsContextProvider>
-      <div className="flex h-full flex-col overflow-hidden">
-        {has.data ? (
-          <>
-            <BlockedBanner />
-            <InviteBotBanner />
-            <div className="flex grow flex-row overflow-auto">
-              {userPermissions.canRead ? (
-                <ManageServer>{children}</ManageServer>
-              ) : (
-                <ForbiddenPage />
-              )}
-            </div>
-          </>
-        ) : (
-          <InviteBotPage />
-        )}
-      </div>
+      <LayoutInner>{children}</LayoutInner>
     </UserPermissionsContextProvider>
   );
 }
---
+++ apps/website/src/app/dashboard/servers/[guildId]/layoutInner.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useContext } from "react";
+import { useParams } from "next/navigation";
+
+import type { DashboardGuildParams } from "./layout";
+import { api } from "~/trpc/react";
+import { BlockedBanner } from "./BlockedBanner";
+import { ForbiddenPage } from "./ForbiddenPage";
+import { InviteBotBanner } from "./InviteBotBanner";
+import { InviteBotPage } from "./InviteBotPage";
+import { ManageServer } from "./ManageServer";
+import { UserPermissionsContext } from "./UserPermissionsContext";
+
+export default function LayoutInner({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { guildId } = useParams<DashboardGuildParams>();
+  const userPermissions = useContext(UserPermissionsContext);
+
+  const has = api.guild.has.useQuery({
+    discordGuildId: guildId,
+  });
+
+  if (!has.isSuccess) return;
+
+  return (
+    <div className="flex h-full flex-col overflow-hidden">
+      {has.data ? (
+        <>
+          <BlockedBanner />
+          <InviteBotBanner />
+          <div className="flex grow flex-row overflow-auto">
+            {userPermissions.canRead ? (
+              <ManageServer>{children}</ManageServer>
+            ) : (
+              <ForbiddenPage />
+            )}
+          </div>
+        </>
+      ) : (
+        <InviteBotPage />
+      )}
+    </div>
+  );
+}
