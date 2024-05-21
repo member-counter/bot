f2f9e2484eb2675eff9cd597e02fde557b9ea3e9
eduardozgz <eduardozgz@protonmail.com>
2024-05-21 17:49:20 +0000

    guild settings page done

--- apps/website/src/app/dashboard/servers/[guildId]/settings/page.tsx
+++ apps/website/src/app/dashboard/servers/[guildId]/settings/page.tsx
@@ -4,19 +4,30 @@ import { useContext, useEffect, useId, useState } from "react";
 import { useParams } from "next/navigation";
 import { SaveIcon, ServerCogIcon } from "lucide-react";
 
+import { cn } from "@mc/ui";
 import { Button } from "@mc/ui/button";
 import { Checkbox } from "@mc/ui/checkbox";
+import { Label } from "@mc/ui/label";
 import { Separator } from "@mc/ui/separator";
 
 import type { DashboardGuildParams } from "../layout";
+import AutocompleteInput from "~/app/components/AutocompleteInput";
+import { searchableLocales } from "~/other/locales";
 import { api } from "~/trpc/react";
 import { MenuButton } from "../../../MenuButton";
 import { LoadingPage } from "../LoadingPage";
+import {
+  AutocompleteLocaleItemRenderer,
+  localeItemRenderer,
+} from "../TemplateEditor/DataSource/Format/DataSourceFormat";
+import DataSourceFormatDigitInput from "../TemplateEditor/DataSource/Format/DataSourceFormatDigitInput";
 import { UserPermissionsContext } from "../UserPermissionsContext";
 import { ResetSettings } from "./ResetButton";
 
 export default function Page() {
   const compactNotationCheckbox = useId();
+  const localeInput = useId();
+  const [guildSettingsDataId, setGuildSettingsDataId] = useState(0);
   const userPermissions = useContext(UserPermissionsContext);
   const { guildId } = useParams<DashboardGuildParams>();
   const guildSettingsMutation = api.guild.update.useMutation();
@@ -31,6 +42,7 @@ export default function Page() {
     if (!guildSettings) return;
     if (isDirty) return;
     _setMutableGuildSettings(structuredClone(guildSettings));
+    setGuildSettingsDataId(Math.random());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [guildSettings]);
 
@@ -42,16 +54,18 @@ export default function Page() {
   };
 
   const save = async () => {
-    await guildSettingsMutation.mutateAsync({
-      ...mutableGuildSettings,
-    });
+    _setMutableGuildSettings(
+      await guildSettingsMutation.mutateAsync({
+        ...mutableGuildSettings,
+      }),
+    );
     setIsDirty(false);
   };
 
   return (
     <div className="flex h-full flex-col">
       <div className="flex h-[48px] w-full flex-shrink-0 flex-row items-center justify-center pl-3 pr-1 font-semibold">
-        <ServerCogIcon className="mr-3 h-5 w-5" />
+        <ServerCogIcon className="mr-3 h-5 w-5 sm:hidden" />
         <div>Server Settings</div>
         <MenuButton />
       </div>
@@ -59,7 +73,7 @@ export default function Page() {
       <div className="grow overflow-hidden">
         <form
           action={save}
-          className="m-auto flex h-full  flex-col  gap-3 overflow-auto p-3 sm:max-w-[500px]"
+          className="m-auto flex h-full  flex-col  gap-5 overflow-auto p-3 sm:max-w-[500px]"
           onInput={() => setIsDirty(true)}
         >
           <Checkbox
@@ -78,6 +92,75 @@ export default function Page() {
           >
             Use compact notation for numbers
           </Checkbox>
+          <div className="flex flex-col gap-3">
+            <Label htmlFor={localeInput}>Locale</Label>
+            {mutableGuildSettings.formatSettings.locale &&
+              [mutableGuildSettings.formatSettings.locale].map(
+                localeItemRenderer({
+                  remove: () =>
+                    setMutableGuildSettings({
+                      ...mutableGuildSettings,
+                      formatSettings: {
+                        ...mutableGuildSettings.formatSettings,
+                        locale: "",
+                      },
+                    }),
+                  update: (locale) =>
+                    setMutableGuildSettings({
+                      ...mutableGuildSettings,
+                      formatSettings: {
+                        ...mutableGuildSettings.formatSettings,
+                        locale,
+                      },
+                    }),
+                }),
+              )}
+            {!mutableGuildSettings.formatSettings.locale && (
+              <AutocompleteInput
+                itemRenderer={AutocompleteLocaleItemRenderer}
+                placeholder="Search locale..."
+                onAdd={(locale) => {
+                  setMutableGuildSettings({
+                    ...mutableGuildSettings,
+                    formatSettings: {
+                      ...mutableGuildSettings.formatSettings,
+                      locale,
+                    },
+                  });
+                }}
+                suggestLimit={10}
+                suggestableItems={searchableLocales}
+                allowSearchedItem={true}
+              />
+            )}
+          </div>
+          <div className="flex flex-col gap-3">
+            <Label>Custom digits</Label>
+            <div className="grid grid-cols-3 gap-1.5">
+              {new Array(10).fill("").map((_, i) => {
+                return (
+                  <DataSourceFormatDigitInput
+                    key={i}
+                    className={cn({ "col-span-3": i === 0 })}
+                    initialValue={
+                      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+                      mutableGuildSettings.formatSettings.digits[i] ||
+                      i.toString()
+                    }
+                    readAgainInitialValue={guildSettingsDataId}
+                    onChange={(digit) => {
+                      mutableGuildSettings.formatSettings.digits[i] =
+                        digit || i.toString();
+                      setMutableGuildSettings({
+                        ...mutableGuildSettings,
+                      });
+                    }}
+                    placeholder={i.toString()}
+                  />
+                );
+              })}
+            </div>
+          </div>
           <div className="mt-auto flex flex-col gap-3 sm:flex-row-reverse">
             <Button
               className="w-auto"
@@ -89,9 +172,15 @@ export default function Page() {
                 guildSettingsMutation.isPending
               }
             >
-              {guildSettingsMutation.isSuccess && !isDirty ? "Saved" : "Save"}
+              {isDirty ? "Save" : "Saved"}
             </Button>
-            <ResetSettings className="w-auto sm:mr-auto" guildId={guildId} />
+            <ResetSettings
+              className="w-auto sm:mr-auto"
+              guildId={guildId}
+              disabled={
+                !userPermissions.canModify || guildSettingsMutation.isPending
+              }
+            />
           </div>
         </form>
       </div>
