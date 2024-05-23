c5ad0b9cee269ae2f4dccbe6d776f0fe171cd535
eduardozgz <eduardozgz@protonmail.com>
2024-05-23 15:05:59 +0000

    5s is too slow

--- packages/bot-data-exchange/src/env.ts
+++ packages/bot-data-exchange/src/env.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production", "test"]),
-    BDE_CALL_TIMEOUT: z.coerce.number().default(5000),
+    BDE_CALL_TIMEOUT: z.coerce.number().default(2000),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
