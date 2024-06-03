448cde2e27ae804649a8da14b0f210158c292259
eduardozgz <eduardozgz@protonmail.com>
2024-06-03 14:22:50 +0000

    bug fix, add more env variables

--- apps/bot/src/env.ts
+++ apps/bot/src/env.ts
@@ -4,10 +4,27 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production", "test"]),
+    DISCORD_CLIENT_ID: z.string(),
     DISCORD_BOT_TOKEN: z.string(),
-    DISCORD_BOT_IS_PREMIUM: z.coerce.boolean(),
-    DISCORD_BOT_IS_PRIVILEGED: z.coerce.boolean(),
+    DISCORD_BOT_IS_PREMIUM: z
+      .string()
+      .toLowerCase()
+      .transform((x) => x === "true")
+      .pipe(z.boolean()),
+    DISCORD_BOT_IS_PRIVILEGED: z
+      .string()
+      .toLowerCase()
+      .transform((x) => x === "true")
+      .pipe(z.boolean()),
     WEBSITE_URL: z.string(),
+    NEXT_PUBLIC_BOT_REPO_URL: z.string(),
+    NEXT_PUBLIC_SUPPORT_URL: z.string(),
+    AUTO_DEPLOY_COMMANDS: z
+      .string()
+      .toLowerCase()
+      .transform((x) => x === "true")
+      .pipe(z.boolean()),
+    DEPLOY_COMMANDS_TO_GUILD_ID: z.string().optional(),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
