5e1d540e39547125a0a9e6cb00cb3af546e17d06
eduardozgz <eduardozgz@protonmail.com>
2024-06-09 21:34:18 +0000

    refactor

--- apps/bot/src/DataSourceService/DataSourceEvaluator/DataSourceEvaluationError.ts
+++ apps/bot/src/DataSourceService/DataSourceEvaluator/DataSourceEvaluationError.ts
@@ -1,14 +1,15 @@
-enum DataSourceEvaluationErrorType {
-  UNKNOWN,
-  UNKNOWN_DATA_SOURCE,
-  UNKNOWN_EVALUATION_RETURN_TYPE,
-  FAILED_TO_RETURN_A_FINAL_STRING,
-  DELIMITED_DATA_SOURCE_IS_ILLEGAL_JSON,
-  DELIMITED_DATA_SOURCE_IS_INVALID,
-}
+export const DataSourceEvaluationErrorTypes = [
+  "UNKNOWN",
+  "UNKNOWN_DATA_SOURCE",
+  "UNKNOWN_EVALUATION_RETURN_TYPE",
+  "FAILED_TO_RETURN_A_FINAL_STRING",
+  "DELIMITED_DATA_SOURCE_IS_ILLEGAL_JSON",
+  "DELIMITED_DATA_SOURCE_IS_INVALID",
+  "EVALUATION_RESULT_FOR_CHANNEL_NAME_IS_LESS_THAN_2_CHARACTERS",
+] as const;
 
 export class DataSourceEvaluationError extends Error {
-  constructor(public type: keyof typeof DataSourceEvaluationErrorType) {
-    super(type);
+  constructor(message: (typeof DataSourceEvaluationErrorTypes)[number]) {
+    super(message);
   }
 }
