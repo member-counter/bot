66a5e46e7c7600896df61be6492a9e96b2d41fbf
eduardozgz <eduardozgz@protonmail.com>
2024-06-15 14:41:02 +0000

    Bug fix

--- apps/bot/src/events/guildCreate.ts
+++ apps/bot/src/events/guildCreate.ts
@@ -1,10 +1,16 @@
 import { GuildSettings } from "@mc/common/GuildSettings";
+import { redis } from "@mc/redis";
 
+import { advertiseEvaluatorPrioritykey } from "~/jobs/advertise";
 import { EventHandler } from "../structures";
 
 export const guildCreateEvent = new EventHandler({
   name: "guildCreate",
   handler: async (guild) => {
     await GuildSettings.upsert(guild.id);
+    await redis.advertiseEvaluatorPriority(
+      advertiseEvaluatorPrioritykey(guild.id),
+      guild.client.botInstanceOptions.dataSourceComputePriority.toString(),
+    );
   },
 });
