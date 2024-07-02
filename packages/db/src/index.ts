33780220b81fd6e850c85abd87da14821d09d6b7
eduardozgz <eduardozgz@protonmail.com>
2024-07-02 17:46:39 +0000

    Enable db logging again

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,8 +4,8 @@ import { env } from "./env";
 
 const createPrismaClient = () =>
   new PrismaClient({
-    // log:
-    //   env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log:
+      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
