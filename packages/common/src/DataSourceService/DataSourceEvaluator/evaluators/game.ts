51113fa2b084ee5d5cf5af51f953ee666a91bdbb
eduardozgz <eduardozgz@protonmail.com>
2024-07-13 10:58:16 +0000

    Upgrade gamedig and its types

--- packages/common/src/DataSourceService/DataSourceEvaluator/evaluators/game.ts
+++ packages/common/src/DataSourceService/DataSourceEvaluator/evaluators/game.ts
@@ -6,7 +6,6 @@ import { DataSourceId } from "@mc/common/DataSource";
 import { DataSourceEvaluator } from "..";
 import { DataSourceError } from "../DataSourceError";
 
-// TODO add support again for `ragemp` (https://cdn.rage.mp/master/) and `minecraft-alt` (https://api.mcsrvstat.us/2/${hostname}) in a fork of node-gamedig
 export const gameEvaluator = new DataSourceEvaluator({
   id: DataSourceId.GAME,
   execute: async ({ options }) => {
