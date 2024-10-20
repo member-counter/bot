a903b21c446492aeae0e4f0b969140082570f92b
eduardozgz <eduardozgz@protonmail.com>
2024-10-20 15:33:45 +0000

    Add loading bar

---
+++ apps/website/src/app/components/ProgressBarProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+
+const ProgressBarProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <ProgressBar
+        height="3px"
+        color="#d5510d"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+      {children}
+    </>
+  );
+};
+
+export default ProgressBarProvider;
