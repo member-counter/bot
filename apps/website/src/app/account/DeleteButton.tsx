2d0ae2108515fa843bbcf5ca2fda6d3565b81799
eduardozgz <eduardozgz@protonmail.com>
2024-10-20 16:09:30 +0000

    use next-nprogress-bar router

--- apps/website/src/app/account/DeleteButton.tsx
+++ apps/website/src/app/account/DeleteButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next-nprogress-bar';
 import { TrashIcon } from "lucide-react";
 import { Trans } from "react-i18next";
 
--- apps/website/src/app/legal/PageSwitcher.tsx
+++ apps/website/src/app/legal/PageSwitcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { LegalPagesSlugs } from "@mc/common/Routes";
-import { useRouter, useSearchParams } from "next/navigation";
+import {   useSearchParams } from "next/navigation";
 import { z } from "zod";
 
 import { legalPagesSlugs } from "@mc/common/Routes";
@@ -11,6 +11,7 @@ import { TypographyH1 } from "@mc/ui/TypographyH1";
 
 import { Routes } from "~/other/routes";
 import { legalPages } from "./legalPages";
+import { useRouter } from "next-nprogress-bar";
 
 export default function PageSwitcher() {
   const searchParams = useSearchParams();
