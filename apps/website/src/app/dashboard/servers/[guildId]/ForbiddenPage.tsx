9100e4f112afee20ea133d7fcd8828cd69e7955d
eduardozgz <eduardozgz@protonmail.com>
2024-12-22 17:29:56 +0100

    align

--- apps/website/src/app/dashboard/servers/[guildId]/ForbiddenPage.tsx
+++ apps/website/src/app/dashboard/servers/[guildId]/ForbiddenPage.tsx
@@ -18,7 +18,7 @@ export function ForbiddenPage() {
   return (
     <div className="flex h-full grow flex-col p-1">
       <MenuButton />
-      <div className="m-auto flex flex-col gap-2 p-3 sm:flex-row">
+      <div className="m-auto flex flex-col items-center gap-2 p-3 sm:flex-row">
         <BanIcon className="mx-auto my-4 h-20 w-20 sm:mx-0 sm:my-0 sm:mr-2 sm:h-12 sm:w-12" />
         <p>
           {t("pages.dashboard.servers.forbiddenPage.message", {
