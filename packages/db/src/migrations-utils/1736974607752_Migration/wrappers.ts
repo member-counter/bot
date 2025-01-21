3f6e0c0eb76ae5675e12f891eccb1542b6b84314
eduardozgz <eduardozgz@protonmail.com>
2025-01-21 19:50:37 +0100

    simplify results when possible

--- packages/db/src/migrations-utils/1736974607752_Migration/wrappers.ts
+++ packages/db/src/migrations-utils/1736974607752_Migration/wrappers.ts
@@ -4,17 +4,23 @@ import { DataSourceId } from "./types/DataSource";
 export const toConcat = (
   items: (DataSource | string)[],
   separator?: string | null,
-): DataSourceConcat => ({
-  id: DataSourceId.CONCAT,
-  options: {
-    strings:
-      typeof separator === "string"
-        ? items.flatMap((element, index) =>
-            index < items.length - 1 ? [element, separator] : [element],
-          )
-        : items,
-  },
-});
+): DataSourceConcat | string => {
+  if (items.every((item) => typeof item === "string")) {
+    return items.join(separator ?? "");
+  }
+
+  return {
+    id: DataSourceId.CONCAT,
+    options: {
+      strings:
+        typeof separator === "string"
+          ? items.flatMap((element, index) =>
+              index < items.length - 1 ? [element, separator] : [element],
+            )
+          : items,
+    },
+  };
+};
 
 export const toNumber = (number: DataSource | string): DataSource | number => {
   const parsedNumber = Number(number);
