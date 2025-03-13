fe493a1e5290f4a4cbab2b12bafd0fc16ec9f234
eduardozgz <eduardozgz@protonmail.com>
2025-03-13 15:47:49 +0100

    take error id from otel tracer

---
+++ apps/bot/src/Constants.ts
@@ -0,0 +1,1 @@
+export const OTEL_TRACER_NAME = "@mc/botz";
--- apps/bot/src/events/interactionCreate.ts
+++ apps/bot/src/events/interactionCreate.ts
@@ -1,4 +1,3 @@
-import { randomUUID } from "crypto";
 import type { i18n } from "i18next";
 import {
   ActionRowBuilder,
@@ -13,6 +12,7 @@ import { DiscordBrandingColors as Colors } from "@mc/common/Constants";
 import { KnownError } from "@mc/common/KnownError/index";
 
 import { env } from "~/env";
+import { tracer } from "~/otelTracer";
 import { initI18n } from "../i18n";
 import handleCommand from "../interactions/commands";
 import { BaseEmbed } from "../utils/BaseMessageEmbed";
@@ -21,73 +21,77 @@ export const interactionCreateEvent = new EventHandler({
   name: "interactionCreate",
   handler: async (interaction) => {
     const { logger } = interaction.client.botInstanceOptions;
-    try {
-      switch (interaction.type) {
-        case InteractionType.ApplicationCommand: {
-          if (interaction.isCommand()) {
-            await handleCommand(interaction);
+    await tracer.startActiveSpan(`Interaction handler`, async (span) => {
+      try {
+        switch (interaction.type) {
+          case InteractionType.ApplicationCommand: {
+            if (interaction.isCommand()) {
+              await handleCommand(interaction);
+            }
+            break;
           }
-          break;
         }
-      }
-    } catch (error) {
-      if (interaction.isRepliable()) {
-        const embed = new BaseEmbed(interaction.client);
-        const id = randomUUID();
-        let i18n: i18n | undefined = undefined;
-        let title: string, description: string, supportServerBtn: string;
+      } catch (error) {
+        if (interaction.isRepliable()) {
+          const { traceId } = span.spanContext();
+          const embed = new BaseEmbed(interaction.client);
+          let i18n: i18n | undefined = undefined;
+          let title: string, description: string, supportServerBtn: string;
 
-        try {
-          i18n = await initI18n(interaction);
-          title = i18n.t("interaction.commandHandler.error.title");
-          description = i18n.t("interaction.commandHandler.error.description");
-          supportServerBtn = i18n.t(
-            "interaction.commands.invite.joinSupportServer",
+          try {
+            i18n = await initI18n(interaction);
+            title = i18n.t("interaction.commandHandler.error.title");
+            description = i18n.t(
+              "interaction.commandHandler.error.description",
+            );
+            supportServerBtn = i18n.t(
+              "interaction.commands.invite.joinSupportServer",
+            );
+          } catch (e) {
+            logger.error(e);
+            title = "ERROR!";
+            description =
+              "Something went wrong, please, try again later\n\nError ID: {{ERROR_ID}}";
+            supportServerBtn = "Join support server";
+          }
+
+          embed.setDescription(
+            description.replaceAll("{{ERROR_ID}}", inlineCode(traceId)),
           );
-        } catch (e) {
-          logger.error(e);
-          title = "ERROR!";
-          description =
-            "Something went wrong, please, try again later\n\nError ID: {{ERROR_ID}}";
-          supportServerBtn = "Join support server";
+
+          if (error instanceof KnownError && i18n) {
+            embed.setDescription(i18n.t(`knownErrors.${error.message}`));
+          }
+
+          logger.error(`Interaction error`, { error, interaction });
+
+          embed.setColor(Colors.Red);
+          embed.setTitle(title);
+
+          const componentRow = new ActionRowBuilder<ButtonBuilder>();
+          componentRow.addComponents(
+            new ButtonBuilder({
+              style: ButtonStyle.Link,
+              url: env.NEXT_PUBLIC_SUPPORT_URL,
+              label: supportServerBtn,
+            }),
+          );
+
+          interaction
+            .editReply({
+              embeds: [embed],
+              components: [componentRow],
+            })
+            .catch((error) => {
+              logger.error("Interaction error reply error:", error);
+            });
+        } else {
+          logger.error(
+            "Something went wrong while processing the interaction: ",
+            error,
+          );
         }
-
-        embed.setDescription(
-          description.replaceAll("{{ERROR_ID}}", inlineCode(id)),
-        );
-
-        if (error instanceof KnownError && i18n) {
-          embed.setDescription(i18n.t(`knownErrors.${error.message}`));
-        }
-
-        logger.error(`Interaction error`, { error, interaction });
-
-        embed.setColor(Colors.Red);
-        embed.setTitle(title);
-
-        const componentRow = new ActionRowBuilder<ButtonBuilder>();
-        componentRow.addComponents(
-          new ButtonBuilder({
-            style: ButtonStyle.Link,
-            url: env.NEXT_PUBLIC_SUPPORT_URL,
-            label: supportServerBtn,
-          }),
-        );
-
-        interaction
-          .editReply({
-            embeds: [embed],
-            components: [componentRow],
-          })
-          .catch((error) => {
-            logger.error("Interaction error reply error:", error);
-          });
-      } else {
-        logger.error(
-          "Something went wrong while processing the interaction: ",
-          error,
-        );
       }
-    }
+    });
   },
 });
