65143246fa29602f4cfabbf483816b10fc8b81cb
eduardozgz <eduardozgz@protonmail.com>
2026-01-20 19:29:37 +0100

    use zod v4 for env schemas

--- apps/website/src/env.js
+++ apps/website/src/env.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-properties */
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { z } from "zod/v4";
 
 export const env = createEnv({
   shared: {
